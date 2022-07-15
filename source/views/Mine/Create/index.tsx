137e57a9e05cdb91696fd41bbf6124d60127b63f
杨一 <you@example.com>
2022-07-15 18:28:18 +0800

    fix: 引入swiper

--- source/views/Mine/Create/index.tsx
+++ source/views/Mine/Create/index.tsx
@@ -22,12 +22,31 @@ import { useDynamicColumns } from './components/CreatePrejectTableColum'
 import { OptionalFeld } from '@/components/OptionalFeld'
 import Preijct from './components/Preijct'
 import Need from './components/Need'
+import SwiperCard from './components/SwiperCard'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
 
+// Import Swiper styles
+import 'swiper/css'
+
+const SwiperWrap = styled.div`
+  box-sizing: border-box;
+  padding: 24px;
+  /* width: 1500px; */
+  /* min-width: 1400px; */
+  height: 144px;
+  background-color: #f5f7fa;
+  /* display: flex; */
+  /* align-items: center; */
+  /* justify-content: space-evenly; */
+  /* overflow: hidden; */
+`
 const tabsList = [
   { name: '创建的项目', type: 1, path: 'project' },
   { name: '创建的需求', type: 2, path: 'need' },
 ]
 export default () => {
+  const [swiperActive, setSwiperActive] = useState(1)
   const [active, setActive] = useState(1)
   const navigate = useNavigate()
   useEffect(() => {}, [])
@@ -36,8 +55,32 @@ export default () => {
     // navigate(`/create?type=${value.path}`)
   }
   return (
-    <>
+    <div style={{ border: '1px solid red' }}>
       <StaffHeader>我创建的</StaffHeader>
+      <SwiperWrap>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={6}
+          grabCursor
+          freeMode
+          observer
+          observeParents
+        >
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map(item => (
+            <SwiperSlide key={item}>
+              <div
+                onClick={() => setSwiperActive(item)}
+                style={{
+                  width: '162px',
+                  border: swiperActive === item ? '1px solid red' : '',
+                }}
+              >
+                <SwiperCard></SwiperCard>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </SwiperWrap>
       <TabsHehavior>
         {tabsList.map(i => (
           <div key={i.type} onClick={() => switchTo(i)} className={tabCss}>
@@ -50,6 +93,6 @@ export default () => {
       </TabsHehavior>
       {active === 1 && <Need></Need>}
       {active === 2 && <Preijct></Preijct>}
-    </>
+    </div>
   )
 }
