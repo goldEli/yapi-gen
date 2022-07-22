c940f749e73d031c52b70a279feb23bdaaba6351
文婷 <2547274792@qq.com>
2022-07-22 16:37:24 +0800

    style: 接口文档对接

--- source/services/project/iterate.ts
+++ source/services/project/iterate.ts
@@ -1,1 +1,170 @@
+/* eslint-disable camelcase */
+/* eslint-disable @typescript-eslint/naming-convention */
 import * as http from '@/tools/http'
+
+export const getIterateList: any = async (params: any) => {
+
+  //   const response: any = await http.get<any>('getIterateList', {
+  //     search: {
+  //       project_id: params.projectId,
+  //       name: params.name,
+  //       created_at: params.time,
+  //       status: params.status,
+  //     },
+  //     pagesize: params.pagesize,
+  //     page: params.page,
+  //     orderkey: params.orderkey,
+  //     order: params.order,
+  //   })
+  const response: any = {}
+  response.data = {
+    list: [
+      {
+        id: '1',
+        start_at: '2022-01-12',
+        end_at: '2011-12-12',
+        story_count: 12,
+        story_finish_count: 21,
+        status: 1,
+        name: '敏捷',
+      },
+      {
+        id: '2',
+        start_at: '2022-01-12',
+        end_at: '2011-12-12',
+        story_count: 12,
+        story_finish_count: 21,
+        status: 1,
+        name: '敏捷',
+      },
+    ],
+    pager: {
+      total: 20,
+      page: 2,
+      pagesize: 12,
+    },
+  }
+  return {
+    currentPage: 1,
+    total: response.data.pager.total,
+    list: response.data.list.map((i: any) => ({
+      id: i.id,
+      status: i.status,
+      name: i.name,
+      finishCount: i.story_finish_count,
+      storyCount: i.story_count,
+      createdTime: i.created_at,
+      endTime: i.end_at,
+    })),
+  }
+}
+
+export const addIterate: any = async (params: any) => {
+  await http.post<any>('addIterate', {
+    name: params.name,
+    info: params.info,
+    start_at: params.startTime,
+    end_at: params.endTime,
+    project_id: params.projectId,
+  })
+}
+
+export const updateIterate: any = async (params: any) => {
+  await http.patch<any>('editIterate', {
+    name: params.name,
+    info: params.info,
+    start_at: params.startTime,
+    end_at: params.endTime,
+    project_id: params.projectId,
+    id: params.id,
+  })
+}
+
+export const deleteIterate: any = async (params: any) => {
+  await http.delete<any>('deleteIterate', {
+    project_id: params.projectId,
+    id: params.id,
+  })
+}
+
+export const getIterateInfo: any = async (params: any) => {
+
+  //   const response: any = await http.get<any>('getIterateInfo', {
+  //     project_id: params.projectId,
+  //     id: params.id,
+  //   })
+  const response: any = {}
+  response.data = {
+    id: 0,
+    name: '敏捷',
+    info: '描述',
+    start_at: '2022-01-12',
+    end_at: '2022-12-01',
+    story_count: 12,
+    story_finish_count: 21,
+    status: 2,
+  }
+  return response.data
+}
+
+export const getIterateChangeLog: any = async (params: any) => {
+
+  //   const response: any = await http.get<any>('getIterateChangeLog', {
+  //     search: {
+  //       app_id: params.iterateId,
+  //     },
+  //     pagesize: params.pagesize,
+  //     page: params.page,
+  //     orderkey: params.orderkey,
+  //     order: params.order,
+  //   })
+  const response: any = {}
+  response.data = {
+    list: [
+      {
+        id: '1',
+        start_at: '2022-01-12',
+        end_at: '2011-12-12',
+        story_count: 12,
+        story_finish_count: 21,
+        status: 1,
+        name: '敏捷',
+      },
+      {
+        id: '2',
+        start_at: '2022-01-12',
+        end_at: '2011-12-12',
+        story_count: 12,
+        story_finish_count: 21,
+        status: 1,
+        name: '敏捷',
+      },
+    ],
+    pager: {
+      total: 20,
+      page: 2,
+      pagesize: 12,
+    },
+  }
+  return {
+    currentPage: 1,
+    total: response.data.pager.total,
+    list: response.data.list.map((i: any) => ({
+      id: i.id,
+      status: i.status,
+      name: i.name,
+      finishCount: i.story_finish_count,
+      storyCount: i.story_count,
+      createdTime: i.created_at,
+      endTime: i.end_at,
+    })),
+  }
+}
+
+export const updateIterateStatus: any = async (params: any) => {
+  await http.put<any>('updateIterateStatus', {
+    project_id: params.projectId,
+    id: params.id,
+    status: params.status,
+  })
+}
--- source/views/Project/components/MainGrid.tsx
+++ source/views/Project/components/MainGrid.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-array-index-key */
 import ProjectCard from '@/components/ProjectCard'
 import styled from '@emotion/styled'
 import { Space } from 'antd'
@@ -8,6 +7,7 @@ import IconFont from '@/components/IconFont'
 interface Props {
   onChangeOperation(type: string, id: number): void
   onChangeVisible(): void
+  projectList: any
 }
 
 const SpaceWrap = styled(Space)({
@@ -28,200 +28,12 @@ const AddProject = styled.div({
   cursor: 'pointer',
 })
 
-const list = [
-  {
-    id: '121212',
-    url: '',
-    name: '项目名称',
-    person: 6,
-    demand: 8,
-    iteration: 9,
-    progress: '90%',
-    createName: '何飞',
-    status: 0,
-    time: '2022-02-32',
-    endTime: '200-03-12',
-  },
-  {
-    id: '121212',
-    url: '',
-    name: '项目名称',
-    person: 6,
-    demand: 8,
-    iteration: 9,
-    progress: '90%',
-    createName: '何飞',
-    status: 0,
-    time: '2022-02-32',
-    endTime: '200-03-12',
-  },
-  {
-    id: '121212',
-    url: '',
-    name: '项目名称',
-    person: 6,
-    demand: 8,
-    iteration: 9,
-    progress: '90%',
-    createName: '何飞',
-    status: 0,
-    time: '2022-02-32',
-    endTime: '200-03-12',
-  },
-  {
-    id: '121212',
-    url: '',
-    name: '项目名称',
-    person: 6,
-    demand: 8,
-    iteration: 9,
-    progress: '90%',
-    createName: '何飞',
-    status: 0,
-    time: '2022-02-32',
-    endTime: '200-03-12',
-  },
-  {
-    id: '121212',
-    url: '',
-    name: '项目名称',
-    person: 6,
-    demand: 8,
-    iteration: 9,
-    progress: '90%',
-    createName: '何飞',
-    status: 0,
-    time: '2022-02-32',
-    endTime: '200-03-12',
-  },
-  {
-    id: '121212',
-    url: '',
-    name: '项目名称',
-    person: 6,
-    demand: 8,
-    iteration: 9,
-    progress: '90%',
-    createName: '何飞',
-    status: 0,
-    time: '2022-02-32',
-    endTime: '200-03-12',
-  },
-  {
-    id: '121212',
-    url: '',
-    name: '项目名称',
-    person: 6,
-    demand: 8,
-    iteration: 9,
-    progress: '90%',
-    createName: '何飞',
-    status: 0,
-    time: '2022-02-32',
-    endTime: '200-03-12',
-  },
-  {
-    id: '121212',
-    url: '',
-    name: '项目名称',
-    person: 6,
-    demand: 8,
-    iteration: 9,
-    progress: '90%',
-    createName: '何飞',
-    status: 0,
-    time: '2022-02-32',
-    endTime: '200-03-12',
-  },
-  {
-    id: '121212',
-    url: '',
-    name: '项目名称',
-    person: 6,
-    demand: 8,
-    iteration: 9,
-    progress: '90%',
-    createName: '何飞',
-    status: 0,
-    time: '2022-02-32',
-    endTime: '200-03-12',
-  },
-  {
-    id: '121212',
-    url: '',
-    name: '项目名称',
-    person: 6,
-    demand: 8,
-    iteration: 9,
-    progress: '90%',
-    createName: '何飞',
-    status: 0,
-    time: '2022-02-32',
-    endTime: '200-03-12',
-  },
-  {
-    id: '121212',
-    url: '',
-    name: '项目名称',
-    person: 6,
-    demand: 8,
-    iteration: 9,
-    progress: '90%',
-    createName: '何飞',
-    status: 0,
-    time: '2022-02-32',
-    endTime: '200-03-12',
-  },
-  {
-    id: '121212',
-    url: '',
-    name: '项目名称',
-    person: 6,
-    demand: 8,
-    iteration: 9,
-    progress: '90%',
-    createName: '何飞',
-    status: 0,
-    time: '2022-02-32',
-    endTime: '200-03-12',
-  },
-  {
-    id: '121212',
-    url: '',
-    name: '项目名称',
-    person: 6,
-    demand: 8,
-    iteration: 9,
-    progress: '90%',
-    createName: '何飞',
-    status: 0,
-    time: '2022-02-32',
-    endTime: '200-03-12',
-  },
-  {
-    id: '121212',
-    url: '',
-    name: '项目名称',
-    person: 6,
-    demand: 8,
-    iteration: 9,
-    progress: '90%',
-    createName: '何飞',
-    status: 0,
-    time: '2022-02-32',
-    endTime: '200-03-12',
-  },
-]
-
 const MainGrid = (props: Props) => {
   const navigate = useNavigate()
   return (
     <SpaceWrap size={32}>
-      {list.map((item, index) => (
-        <div
-          key={`${item.id}_${index}`}
-          onClick={() => navigate('/Detail/Demand')}
-        >
+      {props.projectList.list?.map((item: any) => (
+        <div key={item.id} onClick={() => navigate('/Detail/Demand')}>
           <ProjectCard
             item={item}
             onChangeOperation={props.onChangeOperation}
