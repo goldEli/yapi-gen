c454285004f4126d88b09271af45d4972952307d
张际洛 <zhangjiluo@ifun.com>
2022-07-27 16:18:23 +0800

    fix: routes alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import profile from './package.json'
 
 export default defineConfig(config => {
+  const env = loadEnv(config.mode, './environments/', '__')
+
   return {
     plugins: [
       react({
@@ -31,9 +33,10 @@ export default defineConfig(config => {
     },
     server: {
       host: '0.0.0.0',
-      port: '2800',
+      port: 8000,
     },
     envDir: './environments/',
     envPrefix: '__',
+    base: env.__URL_ALIAS__,
   }
 })
