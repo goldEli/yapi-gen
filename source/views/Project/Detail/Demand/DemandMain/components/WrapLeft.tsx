ca31af49e4626534aaafd523cd3f781f148e465c
一 yang <627814058@qq.com>
2022-09-25 13:59:36 +0800

    fix:处理基础结构

--- source/views/Project/Detail/Demand/DemandMain/components/WrapLeft.tsx
+++ source/views/Project/Detail/Demand/DemandMain/components/WrapLeft.tsx
@@ -5,6 +5,8 @@ import styled from '@emotion/styled'
 import { Tree, type TreeProps } from 'antd'
 import { useEffect, useState } from 'react'
 import { getTreeList } from '@/services/project/tree'
+import IconFont from '@/components/IconFont'
+import { DataNode } from 'antd/lib/tree'
 
 const Left = styled.div<{ isShowLeft: boolean }>(
   {
@@ -33,11 +35,78 @@ interface Props {
   isShowLeft: boolean
   projectId: any
 }
+const TreeBox = styled.div`
+  width: 220px;
+  height: 40px;
+  background: #f0f4fa;
+  border-radius: 0px 0px 0px 0px;
 
+  display: flex;
+  align-items: center;
+`
+const TreeItem = (props: any) => {
+  console.log(props, 'shjuju')
+
+  return (
+    <TreeBox>
+      <span>{props.name}</span>
+      <span>{props.num}</span>
+      <IconFont type="more" />
+    </TreeBox>
+  )
+}
+const myData = [
+  {
+    title: '1-2',
+    num: 1,
+    key: '0-0',
+    children: [
+      {
+        title: 1233,
+        num: 1,
+        key: '0-0-0',
+        children: [
+          {
+            title: 'leaf',
+            num: 1,
+            key: '0-0-0-0',
+          },
+          {
+            title: 'leaf',
+            num: 1,
+            key: '0-0-0-1',
+          },
+        ],
+      },
+      {
+        title: 'parent 1-1',
+        num: 1,
+        key: '0-0-1',
+        children: [
+          {
+            titleL: 'sss',
+            key: '0-0-1-0',
+          },
+        ],
+      },
+    ],
+  },
+]
+const filterTreeData = (data: any) => {
+  const newData = data.map((item: any) => ({
+    title: <TreeItem {...item} num={item.num} name={item.title} />,
+    children:
+      item.children && item.children.length
+        ? filterTreeData(item.children)
+        : null,
+  }))
+  return newData
+}
 const WrapLeft = (props: Props) => {
+  const treeData: any = filterTreeData(myData)
   const init = async () => {
     const res = await getTreeList({ id: props.projectId })
-    // console.log(res)
+    console.log(res)
   }
 
   useEffect(() => {
@@ -47,6 +116,7 @@ const WrapLeft = (props: Props) => {
   return (
     <Left isShowLeft={props.isShowLeft}>
       <TitleWrap>需求分类</TitleWrap>
+      <Tree treeData={treeData} />
     </Left>
   )
 }
