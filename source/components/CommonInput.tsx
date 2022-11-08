d3a8cf36d7931a36a271263d50c13f9291d913dc
文婷 <2547274792@qq.com>
2022-11-08 18:09:22 +0800

    style: bug修改

---
+++ source/components/CommonInput.tsx
@@ -0,0 +1,54 @@
+import styled from '@emotion/styled'
+import { Input } from 'antd'
+import { useEffect } from 'react'
+import IconFont from './IconFont'
+
+const MyInput = styled(Input)`
+  font-size: 14px;
+  width: 240px;
+  height: 32px;
+  background: rgba(245, 246, 247, 1);
+  background-blend-mode: normal;
+  mix-blend-mode: normal;
+  display: flex;
+  justify-content: flex-start;
+
+  padding: 5px 12px 5px 12px;
+  border: 1px solid white;
+  input {
+    background: rgba(245, 246, 247, 1);
+    &::placeholder {
+      font-size: 14px;
+    }
+  }
+`
+
+interface Props {
+  onChangeSearch?(val: string): void
+  placeholder: string
+}
+
+const CommonInput = (props: Props) => {
+  useEffect(() => {
+    document
+      .getElementsByClassName('anticon-close-circle')[0]
+      ?.addEventListener('click', () => props.onChangeSearch?.(''))
+    return document
+      .getElementsByClassName('anticon-close-circle')[0]
+      ?.removeEventListener('click', () => props.onChangeSearch?.(''))
+  }, [])
+
+  return (
+    <MyInput
+      onPressEnter={(e: any) => props.onChangeSearch?.(e.target.value)}
+      onBlur={(e: any) => props.onChangeSearch?.(e.target.value)}
+      suffix={
+        <IconFont type="search" style={{ color: '#BBBDBF', fontSize: 16 }} />
+      }
+      placeholder={props.placeholder}
+      allowClear
+    />
+  )
+}
+
+export default CommonInput
--- source/views/MemberInfo/components/MineSwiper.tsx
+++ source/views/MemberInfo/components/MineSwiper.tsx
@@ -4,11 +4,21 @@ import 'swiper/css'
 import { SwiperWrap } from '@/components/StyleCommon'
 import SwiperCard from './SwiperCard'
 import { useTranslation } from 'react-i18next'
+import { message } from 'antd'
 
 const SwiperCardMove = (props: any) => {
   const [t] = useTranslation()
   const { data, onTap } = props
   const [swiperActive, setSwiperActive] = useState('all')
+
+  const onClickProject = (item: any) => {
+    if (item?.is_public !== 1 && !item.user_ismember) {
+      message.warning(t('common.notCheckInfo'))
+    } else {
+      setSwiperActive(item.id)
+      onTap(item.id)
+    }
+  }
 
   return (
     <SwiperWrap>
@@ -37,8 +47,7 @@ const SwiperCardMove = (props: any) => {
               name={item.name}
               avtar={item.cover}
               tap={() => {
-                setSwiperActive(item.id)
-                onTap(item.id)
+                onClickProject(item)
               }}
               show={swiperActive === item.id}
             />
