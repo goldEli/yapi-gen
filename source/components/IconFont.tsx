d18ddb4b2ca3fe8605d2598405a11065c954790e
杨一 <you@example.com>
2022-11-09 15:31:13 +0800

    fix: 改

--- source/components/IconFont.tsx
+++ source/components/IconFont.tsx
@@ -2,7 +2,7 @@ import { createFromIconfontCN } from '@ant-design/icons'
 
 const IconFont = createFromIconfontCN({
   scriptUrl: [
-    'https://lf1-cdn-tos.bytegoofy.com/obj/iconpark/svg_16144_101.163cbb07369297f7b0d8765d3664bd55.js',
+    'https://lf1-cdn-tos.bytegoofy.com/obj/iconpark/svg_16144_102.3e48ec99157aaa4c0e0fef263fc9f0f5.js',
   ],
 })
 
