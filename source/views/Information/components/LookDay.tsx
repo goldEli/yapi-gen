f0defba3d95e9755c42d3a052e4efe40856a941c
杨一 <you@example.com>
2022-11-10 17:46:30 +0800

    fix: 改

--- source/views/Information/components/LookDay.tsx
+++ source/views/Information/components/LookDay.tsx
@@ -49,14 +49,14 @@ const Kong = () => (
 
 const FormWrap = styled.div<{ left: any }>`
   width: 784px;
-  height: 898px;
+  height: 90vh;
   background: #ffffff;
   border-radius: 8px;
   position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%) rotateY(${({ left }) => left}deg);
-  transition: all 2s linear;
+
   box-sizing: border-box;
   padding-left: 24px;
   padding-right: 4px;
@@ -69,7 +69,7 @@ const HiddenWrap = styled.div`
   perspective-origin: center;
   /* overflow: hidden; */
   width: 784px;
-  height: 898px;
+  height: 90vh;
   border-radius: 8px;
   position: absolute;
   left: 50%;
@@ -273,7 +273,7 @@ const LookDay = (props: any) => {
             <div
               ref={messagesEndRef}
               style={{
-                height: '800px',
+                height: 'calc(100% - 100px)',
                 overflow: 'scroll',
                 paddingRight: '24px',
               }}
