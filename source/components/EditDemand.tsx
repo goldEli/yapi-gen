7b5148db5e73ba7272e6191f306fb6668f66e62e
杨一 <you@example.com>
2022-11-10 15:12:23 +0800

    fix: 改

--- source/components/EditDemand.tsx
+++ source/components/EditDemand.tsx
@@ -1059,7 +1059,14 @@ const EditDemand = (props: Props) => {
                     onChangeTag={onChangeTag}
                     isQuick={props.isQuickCreate}
                     addWrap={
-                      <AddWrap hasDash>
+                      <AddWrap
+                        style={{
+                          width: '32px',
+                          paddingLeft: '7px',
+                          boxSizing: 'border-box',
+                        }}
+                        hasDash
+                      >
                         <IconFont type="plus" />
                       </AddWrap>
                     }
