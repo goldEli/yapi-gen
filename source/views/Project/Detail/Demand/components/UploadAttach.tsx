6e46fff1e1c2feb23a2a09d24f0a700660867f8c
杨一 <you@example.com>
2022-12-13 11:39:11 +0800

    fix: 修复评论UI头像压缩

--- source/views/Project/Detail/Demand/components/UploadAttach.tsx
+++ source/views/Project/Detail/Demand/components/UploadAttach.tsx
@@ -98,6 +98,7 @@ export const Card = styled.div`
   position: relative;
   min-width: 372px;
   min-height: 60px;
+  max-width: 100%;
   background: #ffffff;
   box-shadow: 0px 0px 7px 2px rgba(0, 0, 0, 0.04);
   border-radius: 6px 6px 6px 6px;
