0c1245e9b1f50180e7691b99ed6d9c5f43ca6137
杨一 <you@example.com>
2023-01-04 15:29:50 +0800

    fix: 概况useModel移除

--- source/views/Situation/index.tsx
+++ source/views/Situation/index.tsx
@@ -11,11 +11,12 @@ import CompanyModal from '@/components/CompanyModal'
 import Staff from './components/Staff'
 import Need from './components/Need'
 import Iteration from './components/Iteration'
-import { useModel } from '@/models'
+import { useSelector } from '@store/index'
 import PermissionWrap from '@/components/PermissionWrap'
 import { useTranslation } from 'react-i18next'
 import Loading from '@/components/Loading'
 import useSetTitle from '@/hooks/useSetTitle'
+import { getGlobalGeneral } from '@/services/user'
 
 const buttonCss = css``
 const PanelHeaderSecond = styled.div`
@@ -48,7 +49,7 @@ const Situation = () => {
 
   const [t] = useTranslation()
   asyncSetTtile(t('title.general'))
-  const { getGlobalGeneral, userInfo } = useModel('user')
+  const { userInfo } = useSelector((store: { user: any }) => store.user)
   const [companyModalVisible, setCompanyModalVisible] = useState<boolean>(false)
   const [generalData, setGeneralData] = useState<any>()
   const init = async () => {
