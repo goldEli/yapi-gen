9a1185c82ff6d1d4e1fc65171b1ad5b34dd10be6
杨一 <you@example.com>
2023-01-12 10:18:27 +0800

    fix: 修复按钮

--- source/components/CommonModal.tsx
+++ source/components/CommonModal.tsx
@@ -8,6 +8,7 @@ import styled from '@emotion/styled'
 import IconFont from './IconFont'
 import { useTranslation } from 'react-i18next'
 import { CloseWrap } from './StyleCommon'
+import ThrottleButton from './ThrottleButton'
 
 const ModalHeader = styled.div({
   display: 'flex',
@@ -73,9 +74,12 @@ const CommonModal = (props: ModalProps) => {
           {!props.hasFooter && (
             <ModalFooter size={16}>
               <Button onClick={props?.onClose}>{t('common.cancel')}</Button>
-              <Button onClick={props?.onConfirm} type="primary">
+              <ThrottleButton
+                thClick={() => props?.onConfirm!()}
+                type="primary"
+              >
                 {props?.confirmText ? props?.confirmText : t('common.confirm')}
-              </Button>
+              </ThrottleButton>
             </ModalFooter>
           )}
         </>
--- source/components/ThrottleButton.tsx
+++ source/components/ThrottleButton.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
 import { AsyncButton } from '@staryuntech/ant-pro'
 import _ from 'lodash'
 
 const ThrottleButton = (props: any) => {
   return (
-    <AsyncButton onClick={_.throttle(() => props.thClick(), 2000)}>
+    <AsyncButton
+      type="primary"
+      onClick={_.debounce(() => props.thClick(), 1000)}
+    >
       {props.children}
     </AsyncButton>
   )
