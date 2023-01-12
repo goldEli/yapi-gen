d912cbbbc923dc4a6574564105192e6cf8c89542
杨一 <you@example.com>
2023-01-12 11:35:13 +0800

    fix: 修复按钮

--- source/components/ThrottleButton.tsx
+++ source/components/ThrottleButton.tsx
@@ -1,12 +1,13 @@
+import { useThrottle } from '@/hooks/useThrottle'
 import { AsyncButton } from '@staryuntech/ant-pro'
-import _ from 'lodash'
 
 const ThrottleButton = (props: any) => {
+  const tap = useThrottle(() => {
+    props.thClick()
+  }, 1000)
+
   return (
-    <AsyncButton
-      type="primary"
-      onClick={_.debounce(() => props.thClick(), 1000)}
-    >
+    <AsyncButton type="primary" onClick={tap}>
       {props.children}
     </AsyncButton>
   )
---
+++ source/hooks/useThrottle.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable max-statements-per-line */
+import _ from 'lodash'
+import { useRef, useCallback, useEffect } from 'react'
+
+export function useThrottle(cb: any, delay: number) {
+  const options = { leading: true, trailing: false }
+  const cbRef = useRef(cb)
+
+  useEffect(() => {
+    cbRef.current = cb
+  })
+
+  return useCallback(
+    _.throttle((...args) => cbRef.current(...args), delay, options),
+    [delay],
+  )
+}
