535f60658ae55354f565f770aa4f6ed56b48aec8
文婷 <2547274792@qq.com>
2023-02-20 10:22:27 +0800

    style: --

--- source/components/DeleteConfirm.tsx
+++ source/components/DeleteConfirm.tsx
@@ -69,7 +69,7 @@ const DeleteConfirm = (props: Props) => {
       <ModalHeader>
         <Title>
           <IconFont style={{ fontSize: 24, color: '#FA9746' }} type="Warning" />
-          <div>{props.title ? props.title : t('components.deleteConfirm')}</div>
+          <div>{props.title ? props.title : '删除确认'}</div>
         </Title>
         {!props?.notCancel && (
           <CloseWrap width={32} height={32} onClick={props?.onChangeVisible}>
@@ -80,10 +80,10 @@ const DeleteConfirm = (props: Props) => {
       <ModalContent>{props.children ?? props.text}</ModalContent>
       <ModalFooter size={16}>
         {!props?.notCancel && (
-          <Button onClick={props.onChangeVisible}>{t('common.cancel')}</Button>
+          <Button onClick={props.onChangeVisible}>取消</Button>
         )}
         <Button type="primary" onClick={props.onConfirm}>
-          {t('common.confirm')}
+          确定
         </Button>
       </ModalFooter>
     </Modal>
