5c6068befde283b7625713a26acbaa0727ac62ab
杨一 <you@example.com>
2023-02-20 17:32:43 +0800

    fix: 创建项目+反选select组件

---
+++ source/components/MoreSelect/index.tsx
@@ -0,0 +1,66 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { Button, Divider, Select } from 'antd'
+import React, { useMemo, useState } from 'react'
+
+const index = () => {
+  const [items, setItems] = useState(['jack', 'lucy', '1', '2', '3', '4'])
+  const [value, setValue] = useState<string[]>([])
+  const onClear = () => {
+    setValue([])
+  }
+
+  function getArrDifference(arr1: string[], arr2: string[]) {
+    return arr1.concat(arr2).filter((v, i, arr) => {
+      return arr.indexOf(v) === arr.lastIndexOf(v)
+    })
+  }
+  const handleChange = (values: string[]) => {
+    setValue(values)
+  }
+  const invertSelection = () => {
+    const invertSelectionArray = getArrDifference(items, value)
+    setValue(invertSelectionArray)
+  }
+  const prepositionItems = useMemo(() => {
+    if (value.length >= 1) {
+      return value.concat(getArrDifference(items, value))
+    }
+    return items
+  }, [items, value])
+  return (
+    <Select
+      value={value}
+      mode="multiple"
+      onChange={handleChange}
+      placeholder="custom dropdown render"
+      dropdownRender={menu => (
+        <>
+          {menu}
+          <Divider style={{ margin: '8px 0' }} />
+          <div>
+            <Button onClick={onClear}>清空</Button>
+            <Button onClick={invertSelection}>反选</Button>
+          </div>
+        </>
+      )}
+    >
+      {prepositionItems.map(i => (
+        <Select.Option key={i} value={i}>
+          <img
+            src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
+            alt=""
+            style={{
+              width: '24px',
+              height: '24px',
+              borderRadius: '50%',
+              marginRight: '8px',
+            }}
+          />
+          {i}
+        </Select.Option>
+      ))}
+    </Select>
+  )
+}
+
+export default index
