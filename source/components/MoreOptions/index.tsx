d083afbb8eae3190e8a4520500c0d442f3c3176c
杨一 <you@example.com>
2023-03-07 18:15:32 +0800

    fix: 修复下拉

--- source/components/MoreOptions/index.tsx
+++ source/components/MoreOptions/index.tsx
@@ -13,22 +13,35 @@ const MoreOptions = (props: OptionsProps) => {
   return (
     <Wrap>
       {props.type === 'user' && (
-        <>
+        <div
+          style={{
+            height: '15px',
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
           <CommonUserAvatar avatar={props.img} size="small" />
           <WrapText>{props.name}</WrapText>
-        </>
+        </div>
       )}
       {props.type === 'project' && (
-        <>
+        <div
+          style={{
+            height: '15px',
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
           <WrapTextImg src={props.img} />
           <WrapText>{props.name}</WrapText>
-        </>
+        </div>
       )}
       {props.type === 'promise' && (
         <div
           style={{
             display: 'flex',
             flexDirection: 'column',
+            height: '45px',
           }}
         >
           <WrapFirst>{props.name}</WrapFirst>
