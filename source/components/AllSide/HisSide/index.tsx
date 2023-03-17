1a090b11c529f3bbbab45036dedb73942538668a
杨一 <you@example.com>
2023-03-17 16:35:54 +0800

    fix: 返回

--- source/components/AllSide/HisSide/index.tsx
+++ source/components/AllSide/HisSide/index.tsx
@@ -56,7 +56,10 @@ const index = (props: { onGetPermission(value: any): void }) => {
   }
 
   const onGoBack = () => {
-    navigate(`/AdminManagement/StaffManagement`)
+    const params = encryptPhp(
+      JSON.stringify({ id: projectInfo?.id, pageIdx: 'main', type: 1 }),
+    )
+    navigate(`/ProjectManagement/ProjectSetting?data=${params}`)
   }
 
   useEffect(() => {
