9f394e15dbc8967be163c8b14ff583fcbd779413
miaoyu <miaoyu@ifun.com>
2023-03-28 17:51:21 +0800

    feat: schedule 冲突布局

--- source/components/CalendarManager/components/CalendarDay/ScheduleCardList/index.tsx
+++ source/components/CalendarManager/components/CalendarDay/ScheduleCardList/index.tsx
@@ -2,11 +2,13 @@ import React, { useEffect, useMemo } from 'react'
 import { useDispatch, useSelector } from '@store/index'
 import ScheduleCard from '../ScheduleCard'
 import { getScheduleList } from '@store/schedule/schedule.thunk'
+import useCalculationConflict from '../hooks/useCalculationConflict'
 
 interface ScheduleCardListProps {}
 
 const ScheduleCardList: React.FC<ScheduleCardListProps> = props => {
-  const scheduleList = useSelector(store => store.schedule.scheduleList)
+  // const scheduleList = useSelector(store => store.schedule.scheduleList)
+  const { data } = useCalculationConflict()
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -14,10 +16,17 @@ const ScheduleCardList: React.FC<ScheduleCardListProps> = props => {
   }, [])
 
   const content = useMemo(() => {
-    return scheduleList.map(item => {
-      return <ScheduleCard key={item.id} data={item} />
+    return data.map(item => {
+      return (
+        <ScheduleCard
+          key={item.info.id}
+          data={item.info}
+          width={item.width}
+          left={item.left}
+        />
+      )
     })
-  }, [scheduleList])
+  }, [data])
   return <>{content}</>
 }
 
--- source/components/CalendarManager/components/CalendarDay/hooks/useCalculationConflict.ts
+++ source/components/CalendarManager/components/CalendarDay/hooks/useCalculationConflict.ts
@@ -4,9 +4,66 @@
  */
 
 import { useSelector } from '@store/index'
+import { useEffect, useState } from 'react'
+import { getClassifyConflicts, getConflicts, getStyleValue } from '../utils'
 
+const minLeft = 58
 const useCalculationConflict = () => {
   const list = useSelector(store => store.schedule.scheduleList)
+  const [maxWidth, setMaxWidth] = useState(0)
+  const [data, setData] = useState<
+    { info: Model.Schedule.Info; width: number; left: number }[]
+  >([])
+
+  useEffect(() => {
+    const timeScale = document.querySelector('.time-scale')
+    if (!timeScale) {
+      return
+    }
+    const width = Math.floor(getStyleValue(timeScale, 'width')) - minLeft
+    setMaxWidth(width)
+  }, [])
+
+  useEffect(() => {
+    if (!maxWidth) {
+      return
+    }
+    const conflicts = getConflicts(list)
+
+    const conflictsWithSize = conflicts
+      .map(con => {
+        const len = con.length
+        const width = Math.floor((maxWidth - 20) / len)
+        return con.map((item, idx) => {
+          return {
+            id: item.id,
+            width,
+            left: minLeft + width * idx,
+          }
+        })
+      })
+      .flat()
+
+    const d = list.map(item => {
+      const cur = conflictsWithSize.find(i => i.id === item.id)
+      if (cur) {
+        return {
+          info: item,
+          width: cur.width,
+          left: cur.left,
+        }
+      }
+      return {
+        info: item,
+        width: maxWidth,
+        left: minLeft,
+      }
+    })
+
+    setData(d)
+  }, [list, maxWidth, minLeft])
+
+  return { data }
 }
 
 export default useCalculationConflict
