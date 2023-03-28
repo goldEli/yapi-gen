0f5ae112900a92793f59d09390b797769bc4a5e4
miaoyu <miaoyu@ifun.com>
2023-03-28 14:10:22 +0800

    feat: usePosition

--- source/components/CalendarManager/components/CalendarDay/ScheduleCard/index.tsx
+++ source/components/CalendarManager/components/CalendarDay/ScheduleCard/index.tsx
@@ -19,6 +19,7 @@ import { css } from '@emotion/css'
 import { DraggableEvent } from 'react-draggable'
 import { setSchedule } from '@store/schedule'
 import { ResizeDirection } from 're-resizable'
+import usePosition from '../hooks/usePosition'
 
 interface ScheduleCardProps {
   data: Model.Schedule.Info
@@ -51,23 +52,7 @@ const ScheduleCard: React.FC<ScheduleCardProps> = props => {
     endTime: string
   } | null>(null)
 
-  const height = useMemo(() => {
-    const startTimeDayjs = dayjs(startTime)
-    const endTimeDayjs = dayjs(endTime)
-    const hour = endTimeDayjs.hour() - startTimeDayjs.hour()
-    const minute = endTimeDayjs.minute() - startTimeDayjs.minute()
-    const allMinutes = hour * 60 + minute
-    const newHeight = (allMinutes * oneHourHeight) / 60
-    return newHeight
-  }, [startTime, endTime])
-
-  const top = useMemo(() => {
-    const time = dayjs(startTime)
-    const hour = time.hour()
-    const minute = time.minute()
-    const allMinutes = hour * 60 + minute
-    return (allMinutes * oneHourHeight) / 60
-  }, [startTime])
+  const { height, top } = usePosition(startTime, endTime)
 
   const onDrag = (e: DraggableEvent, draggableData: DraggableData) => {
     const { node, y, deltaY, lastY } = draggableData
