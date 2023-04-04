d4f848af174ec7a3868797ce3b703c9a9dae10b9
zhaochunmei <279658732@qq.com>
2023-04-04 10:05:33 +0800

    fix: 模板

--- source/views/Container/components/Side.tsx
+++ source/views/Container/components/Side.tsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled'
 import { useDispatch, useSelector } from '@store/index'
 import { useRef, useState, useEffect } from 'react'
 import ProjectDetailSide from '@/components/AllSide/ProjectDetailSide'
-import FormWorkSide from '@/components/AllSide/FormWorkSide'
+
 import ProjectSide from '@/components/AllSide/ProjectSide'
 import AdminSide from '@/components/AllSide/AdminSide'
 import { useLocation } from 'react-router-dom'
@@ -169,8 +169,7 @@ const Side = (props: { onChangeLeft(value: number): void }) => {
     } else if (String(pathname).includes('/LogManagement')) {
       nodeComponent = <LogSide />
     } else if (String(pathname).includes('/ProjectManagement/')) {
-      // nodeComponent = <ProjectDetailSide />
-      nodeComponent = <FormWorkSide />
+      nodeComponent = <ProjectDetailSide />
     }
 
     return nodeComponent
