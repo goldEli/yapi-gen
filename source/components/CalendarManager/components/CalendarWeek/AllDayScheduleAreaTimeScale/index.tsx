031e43de8632ce31bbfb3656c5aeaa81b45ce694
miaoyu <miaoyu@ifun.com>
2023-04-04 16:23:23 +0800

    feat: 新建全天日程激活状态

--- source/components/CalendarManager/components/CalendarWeek/AllDayScheduleAreaTimeScale/index.tsx
+++ source/components/CalendarManager/components/CalendarWeek/AllDayScheduleAreaTimeScale/index.tsx
@@ -1,12 +1,15 @@
 import styled from '@emotion/styled'
 import React from 'react'
 import classnames from 'classnames'
+import { css } from '@emotion/css'
+import useMaxWidth from '../hooks/useMaxWidth'
 
 interface WeekHeaderProps {}
 
 const Table = styled.table`
   width: 100%;
   height: 100%;
+  position: relative;
   .firstTd {
     width: 58px;
   }
@@ -25,8 +28,30 @@ const Table = styled.table`
     border-right: 1px solid var(--neutral-n6-d1);
   }
 `
+const activeColor = css`
+  background-color: var(--neutral-n6-d1);
+`
+const Title = styled.div<{ left: number }>`
+  color: var(--primary-d1);
+  width: calc((100% - 58px) / 7);
+  font-size: 12px;
+  font-weight: 400;
+  background: var(--function-tag5);
+  height: 22px;
+  position: absolute;
+  left: ${props => props.left + 'px'};
+  top: 0;
+  display: flex;
+  align-items: center;
+  z-index: 100;
+`
 
 const TimeScale: React.FC<WeekHeaderProps> = props => {
+  const [current, setCurrent] = React.useState<number>()
+  const onCreate = (idx: number) => {
+    setCurrent(idx)
+  }
+  const { maxWidth } = useMaxWidth()
   return (
     <Table>
       {Array(4)
@@ -39,8 +64,10 @@ const TimeScale: React.FC<WeekHeaderProps> = props => {
                 .map((item, idx) => {
                   return (
                     <td
+                      onClick={() => onCreate(idx)}
                       className={classnames(
                         'borderRight',
+                        { [activeColor]: idx === current },
                         { borderTop: index === 0 && idx !== 0 },
                         {
                           firstTd: idx === 0,
@@ -53,6 +80,8 @@ const TimeScale: React.FC<WeekHeaderProps> = props => {
             </tr>
           )
         })}
+
+      <Title left={58 + maxWidth * ((current ?? 0) - 1)}>新建日程</Title>
     </Table>
   )
 }
