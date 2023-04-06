70566ca569942d27f16dbecccb5a7fd0c859c3a0
miaoyu <miaoyu@ifun.com>
2023-04-06 17:35:00 +0800

    feat: 月视图 添加颜色控制

--- source/components/CalendarManager/components/CalendarMonth/ScheduleListItem/index.tsx
+++ source/components/CalendarManager/components/CalendarMonth/ScheduleListItem/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import {
+  getColorWithOpacityPointOne,
+  getColor,
+} from '@/components/CalendarManager/utils'
 
 interface ScheduleListItemProps {
   data: Model.Schedule.Info
 }
 
-const ScheduleListItemBox = styled.div<{ bg?: string }>`
+const ScheduleListItemBox = styled.div<{
+  bg?: string
+  hoverBg: string
+  color: string
+}>`
   width: calc(100% - 4px);
   margin-left: 2px;
   margin-right: 2px;
@@ -13,35 +21,46 @@ const ScheduleListItemBox = styled.div<{ bg?: string }>`
   .text {
   }
   &:hover {
-    background: var(--function-tag5);
+    background: ${props => props.hoverBg};
   }
   &:hover .text {
-    color: var(--primary-d1);
+    color: ${props => props.color};
   }
   display: flex;
   gap: 7px;
   align-items: center;
   cursor: pointer;
 `
-const Dot = styled.div<{ bg?: string }>`
+const Dot = styled.div<{ bg: string }>`
   width: 6px;
   height: 6px;
-  background: ${props => (props.bg ? props.bg : 'var(--primary-d1)')};
+  background: ${props => props.bg};
   border-radius: 2px 2px 2px 2px;
 `
 const Time = styled.div`
   color: var(--neutral-n4);
 `
 const Title = styled.div`
+  flex: 1;
   color: var(--neutral-n2);
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `
 
 const ScheduleListItem: React.FC<ScheduleListItemProps> = props => {
   const { data } = props
+  const isAllDay = data.is_all_day === 1 || data.is_span_day
+  // 如果是跨天或者全天任务显示全天
+  const time = isAllDay ? '全天' : data.start_time
   return (
-    <ScheduleListItemBox>
-      <Dot />
-      <Time className="text">{data.start_time}</Time>
+    <ScheduleListItemBox
+      bg={isAllDay ? getColorWithOpacityPointOne(data.color) : void 0}
+      hoverBg={getColorWithOpacityPointOne(data.color)}
+      color={getColor(data.color)}
+    >
+      <Dot bg={getColor(data.color)} />
+      <Time className="text">{time}</Time>
       <Title className="text">{props.data.subject}</Title>
     </ScheduleListItemBox>
   )
