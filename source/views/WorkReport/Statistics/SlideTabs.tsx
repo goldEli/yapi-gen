c05c2d88a04bea9f7f9da90734c2a28747a46053
houxiaoling <houxiaoling@ifun.com>
2023-04-13 15:12:16 +0800

    perf: 样式

--- source/views/WorkReport/Statistics/SlideTabs.tsx
+++ source/views/WorkReport/Statistics/SlideTabs.tsx
@@ -5,9 +5,10 @@ import {
   useMemo,
   useRef,
   useState,
+  useTransition,
 } from 'react'
 import { DoubleRightOutlined, DoubleLeftOutlined } from '@ant-design/icons'
-import { Space } from 'antd'
+import { Divider, Space } from 'antd'
 import styled from '@emotion/styled'
 import { css } from '@emotion/css'
 
@@ -49,8 +50,18 @@ const TabItem = styled.div`
   align-items: center;
   color: ${(props: any) =>
     props.theme ? 'var(--primary-d1)' : 'var(--neutral-n2)'};
-  border-bottom: ${(props: any) =>
-    props.theme ? '2px solid var(--primary-d1)' : 'none'};
+`
+const TabBarLine = styled.div`
+  width: 100%;
+  height: 3px;
+  background: var(--primary-d1);
+  opacity: 1;
+  position: absolute;
+  bottom: 0;
+  visibility: ${(props: any) => (props.theme ? 'visible' : 'hidden')};
+  opacity: ${(props: any) => (props.theme ? '1' : '0')};
+  transition: all 0.2s linear;
+  transition-delay: 0s;
 `
 
 interface SlideTabsProps {
@@ -171,6 +182,7 @@ const SlideTabs: React.FC<SlideTabsProps> = ({
                 onClick={() => handleClick(item.key)}
               >
                 {item.label}
+                <TabBarLine theme={isActive(item.key)} />
               </TabItem>
             ))}
           </Space>
