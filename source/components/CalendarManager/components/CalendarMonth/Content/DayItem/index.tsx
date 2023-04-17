e3019c448a2faf04f352a5a1cd047ceb1797e1e9
miaoyu <miaoyu@ifun.com>
2023-04-17 12:18:34 +0800

    feat: 月视图创建日程

--- source/components/CalendarManager/components/CalendarMonth/Content/DayItem/index.tsx
+++ source/components/CalendarManager/components/CalendarMonth/Content/DayItem/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from '@emotion/styled'
 import { useDispatch, useSelector } from '@store/index'
 import classNames from 'classnames'
@@ -8,6 +8,8 @@ import useCurrentTime from '@/components/CalendarManager/hooks/useCurrentTime'
 import ScheduleList from '../../ScheduleList'
 import {
   resizeMonthSchedule,
+  setQuickCreateScheduleModel,
+  setSelectedDayInMonth,
   // moveMonthSchedule,
   startMoveMonthSchedule,
 } from '@store/calendarPanle'
@@ -81,6 +83,7 @@ const borderRight = css`
 
 const DayItem: React.FC<DayItemProps> = props => {
   const { checkedTime, selectedMonth } = useSelector(store => store.calendar)
+  const { selectedDayInMonth } = useSelector(store => store.calendarPanel)
   const { idx } = props
   const info = selectedMonth?.[props.idx]
   const day = dayjs(info?.date).format('DD')
@@ -88,17 +91,35 @@ const DayItem: React.FC<DayItemProps> = props => {
   const { currentTime } = useCurrentTime()
   const isCurrent = currentTime.isSame(dayjs(info?.datetime), 'day')
   const dispatch = useDispatch()
+
   if (!info) {
     return <></>
   }
+
   return (
     <DayItemBox
       className={classNames({
-        [selectedBg]: isSelected,
+        [selectedBg]: isSelected || selectedDayInMonth === info.datetime,
         [borderRight]: (idx + 1) % 7 === 0,
         [borderBottom]: idx > 27,
       })}
       key={idx}
+      onClick={e => {
+        e.stopPropagation()
+        const target = e.target as HTMLDivElement
+        const { offsetTop, offsetLeft } = target
+        dispatch(
+          setQuickCreateScheduleModel({
+            visible: true,
+            isAll: true,
+            startTime: info.datetime,
+            endTime: info.datetime,
+            x: offsetLeft,
+            y: offsetTop,
+          }),
+        )
+        dispatch(setSelectedDayInMonth(info.datetime))
+      }}
       onMouseEnter={e => {
         console.log('window.calendarPanel.type', window.calendarMonthPanelType)
         if (window.calendarMonthPanelType === 'move') {
