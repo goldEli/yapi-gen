c3983c65dcc7b22d6e56da894852ae9184d958ff
miaoyu <miaoyu@ifun.com>
2023-04-20 11:46:51 +0800

    fix: style

--- source/components/CalendarManager/components/CalendarMonth/Content/index.tsx
+++ source/components/CalendarManager/components/CalendarMonth/Content/index.tsx
@@ -16,7 +16,7 @@ const ContentBox = styled.div`
   flex: 1;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 142px 142px 142px 142px 142px;
   position: relative;
 `
 
