636876839f35d8eaeabd0db71a7090c1c23b3b18
杨一 <you@example.com>
2023-04-25 16:31:16 +0800

    fix: 权限

--- source/views/SiteNotifications/index.tsx
+++ source/views/SiteNotifications/index.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Content } from '../Container'
 import Side from './Side'
-import SlideTabs from '@/components/SlideTabs/SlideTabs'
 
 const Index = () => {
   const [changeLeft, setChangeLeft] = useState(200)
@@ -10,7 +9,6 @@ const Index = () => {
     <Content>
       <Side onChangeLeft={setChangeLeft} />
       <div style={{ flex: '1' }}>
-        {/* <SlideTabs /> */}
         <Outlet />
       </div>
     </Content>
