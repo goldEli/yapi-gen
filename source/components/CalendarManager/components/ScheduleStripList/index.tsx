c66e5bb98e684d2b21995cdce76ba89d69311ade
miaoyu <miaoyu@ifun.com>
2023-04-28 10:55:21 +0800

    fix: 1011396
    # Conflicts:
    #	source/components/CalendarManager/components/ScheduleStripList/index.tsx

--- source/components/CalendarManager/components/ScheduleStripList/index.tsx
+++ source/components/CalendarManager/components/ScheduleStripList/index.tsx
@@ -99,7 +99,7 @@ const ScheduleStripList: React.FC<ScheduleListProps> = props => {
             dispatch(
               setScheduleListModal({
                 visible: true,
-                top: top - boxTop,
+                top: top - boxTop - 72,
                 left: left - boxLeft,
                 date: dayjs(data.date).date(),
                 scheduleListData: allList,
