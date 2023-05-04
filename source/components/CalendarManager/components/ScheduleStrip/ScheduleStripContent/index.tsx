e127b1099825869b1612a5050b3c4453877f5658
miaoyu <miaoyu@ifun.com>
2023-05-04 11:57:31 +0800

    style: 选中样式

--- source/components/CalendarManager/components/ScheduleStrip/ScheduleStripContent/index.tsx
+++ source/components/CalendarManager/components/ScheduleStrip/ScheduleStripContent/index.tsx
@@ -4,51 +4,50 @@ import { Dot, Time, Title } from '../styled'
 import { getColor } from '@/components/CalendarManager/utils'
 import useShowTime from '@/components/CalendarManager/hooks/useShowTime'
 import IconFont from '@/components/IconFont'
+import useSelectedSchedule from '@/components/CalendarManager/hooks/useSelectedSchedule'
 
 interface ScheduleStripContentProps {
   data?: Model.Schedule.Info
 }
-
-const ScheduleStripContentBox = styled.div`
-  width: 100%;
-`
 
 const ScheduleStripContent: React.FC<ScheduleStripContentProps> = props => {
   const { timeStr } = useShowTime(props?.data)
   const showRedBell = React.useMemo(() => {
     return props.data?.is_show_replay
   }, [props.data])
-  // if (props?.data?.is_show_busy) {
-  //   return (
-  //     <>
-  //       <Time className={getColorClassName()}>{data?.start_time}&nbsp;</Time>
-  //       <Title
-  //         color={color}
-  //         isSelected={isSelected}
-  //         className={getColorClassName()}
-  //       >
-  //         {data?.is_busy_text}
-  //       </Title>
-  //     </>
-  //   )
-  // }
+
   const title = React.useMemo(() => {
     if (props.data?.is_show_busy) {
       return props.data?.is_busy_text
     }
     return props.data?.subject
   }, [props.data])
+
+  const { selected } = useSelectedSchedule(props.data)
+
   return (
     <>
       <Dot bg={getColor(props?.data?.color ?? 0)} />
-      <Time className="text">{timeStr}</Time>
+      <Time
+        selected={selected}
+        selectedColor={getColor(props?.data?.color ?? 0)}
+        className="text"
+      >
+        {timeStr}
+      </Time>
       {showRedBell && (
         <IconFont
           style={{ fontSize: '16px', marginRight: '4px' }}
           type="bell-red"
         />
       )}
-      <Title className="text">{title}</Title>
+      <Title
+        selected={selected}
+        selectedColor={getColor(props?.data?.color ?? 0)}
+        className="text"
+      >
+        {title}
+      </Title>
     </>
   )
 }
