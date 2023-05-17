0f3a7c71f24fad30e7f251272a9db11592a2e994
xuewanyun <xuewanyun@ifun.com>
2023-05-17 14:59:14 +0800

    feat 添加一个冲刺项目入口

--- source/components/TableSelectOptions.tsx
+++ source/components/TableSelectOptions.tsx
@@ -1,27 +1,49 @@
 import React from 'react'
 
 import styled from '@emotion/styled'
+import IconFont from './IconFont'
 const SelectOptions = styled.div`
   position: absolute;
-  top: 6px;
-  left: 10px;
+  top: 30px;
+  left: 0px;
   width: 120px;
-  /* height: 136px; */
   background: #ffffff;
   box-shadow: 0px 0px 15px 6px rgba(0, 0, 0, 0.12);
   border-radius: 6px 6px 6px 6px;
   opacity: 1;
-  padding: 10px 16px;
+  padding: 4px 0px;
+  z-index: 9;
 `
 const SelectItem = styled.div`
-  height: 20px;
+  height: 32px;
+  cursor: pointer;
+  color: var(--neutral-n2);
+  font-size: var(--font14);
+  display: flex;
+  align-items: center;
+  padding-left: 16px;
+  &:hover {
+    background: var(--hover-d3);
+  }
+  .activity {
+    color: var(--primary-d2);
+  }
 `
 interface IProps {}
 const TableSelectOptions = (props: IProps) => {
   return (
     <SelectOptions>
-      {[1, 2, 3].map(item => (
-        <SelectItem>{item}</SelectItem>
+      {['管理员', '编辑者', '参与者'].map((item, index) => (
+        <SelectItem>
+          <span className={index === 1 ? 'activity' : ''}>{item}</span>
+          {index === 1 && (
+            <IconFont
+              type="check"
+              style={{ marginLeft: '30px' }}
+              className={index === 1 ? 'activity' : ''}
+            ></IconFont>
+          )}
+        </SelectItem>
       ))}
     </SelectOptions>
   )
