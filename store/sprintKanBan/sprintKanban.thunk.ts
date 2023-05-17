3e764ddfcb8cda5c9e48cadcd100f5ec45da691e
miaoyu <miaoyu@ifun.com>
2023-05-17 09:41:28 +0800

    feat: add message

--- store/sprintKanBan/sprintKanban.thunk.ts
+++ store/sprintKanBan/sprintKanban.thunk.ts
@@ -2,6 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 import * as services from '@/services'
 import { AppDispatch } from '@store/index'
 import { setSaveAsViewModelInfo } from '.'
+import { getMessage } from '@/components/Message'
 
 const name = 'sprintKanBan'
 
@@ -16,5 +17,7 @@ export const closeSaveAsViewModel = () => async (dispatch: AppDispatch) => {
 // 保存视图
 export const onSaveAsViewModel = () => async (dispatch: AppDispatch) => {
   // TODO
+
+  getMessage({ msg: '保存成功!', type: 'success' })
   dispatch(closeSaveAsViewModel())
 }
