ea68e95ef5a4f556a2bfcf6b8fbcb3c786ab889b
miaoyu <miaoyu@ifun.com>
2023-05-19 17:42:29 +0800

    feat: add StatusListItem

--- source/views/ProjectSetting/components/KanBanSetting/StatusList/index.tsx
+++ source/views/ProjectSetting/components/KanBanSetting/StatusList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from '@emotion/styled'
 import IconFont from '@/components/IconFont'
 import StateTag from '@/components/StateTag'
+import StatusListItem from '../StatusListItem'
 
 interface StatusListProps {}
 
@@ -11,49 +12,6 @@ const StatusListBox = styled.div`
   align-items: center;
   flex-direction: column;
   gap: 8px;
-`
-const StatusListItem = styled.div`
-  width: 100%;
-  height: 44px;
-  background: var(--neutral-white-d1);
-  border-radius: 6px 6px 6px 6px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 16px;
-  box-sizing: border-box;
-`
-const StatusListItemLeft = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 8px;
-`
-
-const ImageIcon = styled.img`
-  width: 20px;
-  height: 20px;
-`
-const Text = styled.div<{ bg: string }>`
-  width: 52px;
-  height: 22px;
-  background: ${props => props.bg};
-  border-radius: 6px 6px 6px 6px;
-  opacity: 1;
-  font-size: 12px;
-  color: var(--neutral-white-d7);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
-
-const Count = styled.div`
-  font-size: 12px;
-  color: var(--neutral-n3);
-`
-
-const IconWarp = styled(IconFont)<{ active: boolean }>`
-  font-size: 16px;
-  color: ${props => (props.active ? 'var(--primary-d1)' : 'var(--neutral-n3)')};
 `
 
 const data: Model.KanbanConfig.Status[] = [
@@ -83,28 +41,7 @@ const StatusList: React.FC<StatusListProps> = props => {
   return (
     <StatusListBox>
       {data.map(item => {
-        return (
-          <StatusListItem key={item.flow_status_id}>
-            <StatusListItemLeft>
-              <IconWarp active type="move" />
-              <ImageIcon src={item.attachment_path} />
-              {/* <Text bg={item.status_color}>{item.status_name}</Text> */}
-              <StateTag
-                name={item.status_name}
-                state={
-                  item?.is_start === 1 && item?.is_end === 2
-                    ? 1
-                    : item?.is_end === 1 && item?.is_start === 2
-                    ? 2
-                    : item?.is_start === 2 && item?.is_end === 2
-                    ? 3
-                    : 0
-                }
-              />
-            </StatusListItemLeft>
-            <Count>{`${item.stories_count}个事务`}</Count>
-          </StatusListItem>
-        )
+        return <StatusListItem key={item.id} data={item} />
       })}
     </StatusListBox>
   )
