2ae5f40913d8b9bcc15e385ddbbf798bcfdf68da
文婷 <2547274792@qq.com>
2023-05-22 16:00:04 +0800

    style: 解决报错

--- source/views/SprintProjectDetail/components/ActivitySprint.tsx
+++ source/views/SprintProjectDetail/components/ActivitySprint.tsx
@@ -1,38 +1,50 @@
 import { Tabs, TabsProps } from 'antd'
 import { ActivityTabItem, InfoItem, ItemNumber, Label } from '../style'
+import { useState } from 'react'
 
 const ActivitySprint = () => {
+  const [activeKey, setActiveKey] = useState('1')
   const items: TabsProps['items'] = [
     {
       key: '1',
       label: (
         <ActivityTabItem>
           <span>Tab 1</span>
-          <ItemNumber>5</ItemNumber>
+          <ItemNumber isActive={activeKey === '1'}>5</ItemNumber>
         </ActivityTabItem>
       ),
       children: `Content of Tab Pane 1`,
     },
     {
       key: '2',
-      label: `Tab 2`,
+      label: (
+        <ActivityTabItem>
+          <span>Tab 1</span>
+          <ItemNumber isActive={activeKey === '2'}>6</ItemNumber>
+        </ActivityTabItem>
+      ),
       children: `Content of Tab Pane 2`,
     },
     {
       key: '3',
-      label: `Tab 3`,
+      label: (
+        <ActivityTabItem>
+          <span>Tab 1</span>
+          <ItemNumber isActive={activeKey === '3'}>8</ItemNumber>
+        </ActivityTabItem>
+      ),
       children: `Content of Tab Pane 3`,
     },
   ]
 
   const onChange = (key: string) => {
-    console.log(key)
+    setActiveKey(key)
   }
 
   return (
     <InfoItem>
       <Label>活动</Label>
-      <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
+      <Tabs defaultActiveKey={activeKey} items={items} onChange={onChange} />
     </InfoItem>
   )
 }
