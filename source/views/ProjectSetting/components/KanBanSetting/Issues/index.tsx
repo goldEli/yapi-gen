9a4ebbd2f8c1255970d491ec98fa0ee00700c439
miaoyu <miaoyu@ifun.com>
2023-05-22 17:08:53 +0800

    feat: 占位

--- source/views/ProjectSetting/components/KanBanSetting/Issues/index.tsx
+++ source/views/ProjectSetting/components/KanBanSetting/Issues/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import styled from '@emotion/styled'
 import { Droppable } from 'react-beautiful-dnd'
 import IssueCard from '../IssueCard'
 import { handleId } from '../utils'
 import StatusList from '../StatusList'
 import StatusListItem from '../StatusListItem'
+import useMaxStatusNum from '../hooks/useMaxStatusNum'
 
 interface IssuesProps {
   issues?: Model.KanbanConfig.Category
@@ -19,6 +20,7 @@ const DropArea = styled.div`
   width: 302px;
   box-sizing: border-box;
   padding: 16px;
+  gap: 8px;
 `
 
 const DropStatusArea = styled.div`
@@ -30,9 +32,18 @@ const DropStatusArea = styled.div`
     border: 1px solid green;
   }
 `
+const StatusListItemPlaceholder = styled.div`
+  width: 100%;
+  height: 44px;
+  padding: 0 16px;
+  box-sizing: border-box;
+  flex-shrink: 0;
+`
 const Issues: React.FC<IssuesProps> = props => {
   const { issues, groupId } = props
   const droppableId = handleId(groupId ?? 0, issues?.id ?? 0)
+  const { placeholderItemsLength } = useMaxStatusNum(issues)
+
   return (
     <Droppable type="STATUS" key={issues?.id} droppableId={droppableId}>
       {(provided, snapshot) => {
@@ -48,6 +59,11 @@ const Issues: React.FC<IssuesProps> = props => {
                 />
               )
             })}
+            {Array(placeholderItemsLength)
+              .fill(0)
+              .map((_, idx) => {
+                return <StatusListItemPlaceholder key={idx} />
+              })}
             {provided.placeholder}
           </DropArea>
         )
