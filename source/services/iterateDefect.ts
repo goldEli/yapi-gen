c1e8d1194ccc5e8315cca7f4cb6b6aa57ec7396a
文婷 <2547274792@qq.com>
2023-05-30 12:11:07 +0800

    style: sprint替换affairs

--- source/services/iterateDefect.ts
+++ source/services/iterateDefect.ts
@@ -1,12 +1,12 @@
 import * as http from '@/tools/http'
 
 // 删除缺陷
-// export const deleteSprint = async (params: {
+// export const deleteAffairs = async (params: {
 //   projectId: number
 //   id: number
 //   isDeleteChild?: number
 // }) => {
-//   await http.post<any>('deleteSprint', {
+//   await http.post<any>('deleteAffairs', {
 //     project_id: params.projectId,
 //     id: params.id,
 //     is_delete_childs: params.isDeleteChild,
