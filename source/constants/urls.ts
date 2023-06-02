c8a0924a0b62ba44f49fa818a59cf4a145d3c564
zhaochunmei <279658732@qq.com>
2023-06-02 17:37:55 +0800

    fix: 导出接口

--- source/constants/urls.ts
+++ source/constants/urls.ts
@@ -300,6 +300,8 @@ const urls = {
   viewsList: '/b/story/views/list',
   // 近期的冲刺项目
   recentCreateData: '/b/project/recentCreateData',
+  // 导出
+  export: 'b/efficiency/member/work_contrast/export',
   /**
    * 看板
    */
