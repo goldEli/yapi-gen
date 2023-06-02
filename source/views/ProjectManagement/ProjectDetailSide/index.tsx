cbd33bf6594f39ee4dfcaece93ee278009a3f51e
xuewanyun <xuewanyun@ifun.com>
2023-06-02 18:07:55 +0800

    feat: 路由跳转

--- source/views/ProjectManagement/ProjectDetailSide/index.tsx
+++ source/views/ProjectManagement/ProjectDetailSide/index.tsx
@@ -48,7 +48,6 @@ const ProjectDetailSide = () => {
   const paramsData = getParamsData(searchParams)
   const projectId = paramsData?.id
   const { projectInfo } = useSelector(store => store.project)
-  const { projectType } = useSelector(state => state.createProject)
   const [selectedKeys, setSelectedKeys] = useState(['ProjectInfo'])
   const routerPath = useLocation()
   const navigate = useNavigate()
