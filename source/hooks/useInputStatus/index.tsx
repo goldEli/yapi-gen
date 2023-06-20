01edce5337c927d2b48622db1be3f0c99e1abaed
杨一 <you@example.com>
2023-06-20 10:35:18 +0800

    fix: 聚焦下不允许快捷键

---
+++ source/hooks/useInputStatus/index.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+
+const useInputStatus = () => {
+  const [isInputFocused, setIsInputFocused] = useState(false)
+
+  useEffect(() => {
+    const handleFocus = () => {
+      setIsInputFocused(true)
+    }
+
+    const handleBlur = () => {
+      setIsInputFocused(false)
+    }
+
+    // 监听全局的输入焦点事件
+    document.addEventListener('focusin', handleFocus)
+    document.addEventListener('focusout', handleBlur)
+
+    return () => {
+      // 在组件卸载时移除事件监听器
+      document.removeEventListener('focusin', handleFocus)
+      document.removeEventListener('focusout', handleBlur)
+    }
+  }, [])
+
+  return isInputFocused
+}
+
+export default useInputStatus
