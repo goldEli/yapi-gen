aaa84543dcbb8a2125678c352f257bcc50f714ad
文婷 <2547274792@qq.com>
2023-06-25 18:11:06 +0800

    style: 顶部切换清除当页参数

--- source/views/IterationDefect/components/DefectTable.tsx
+++ source/views/IterationDefect/components/DefectTable.tsx
@@ -85,6 +85,10 @@ const DefectTable = (props: Props) => {
     }
   }, [tapSort])
 
+  useEffect(() => {
+    setSelectedRowKeys([])
+  }, [projectId])
+
   // 勾选或者取消勾选，显示数量 keys: 所有选择的数量，type： 添加还是移除
   const onOperationCheckbox = (type: any, keys?: any) => {
     const redClassElements = document.getElementsByClassName(
