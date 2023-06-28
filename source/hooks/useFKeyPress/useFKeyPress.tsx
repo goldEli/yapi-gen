d894f483a8056cdf22805077ba8bf4827c5aace4
杨一 <you@example.com>
2023-06-28 11:24:37 +0800

    fix: f键修复

--- source/hooks/useFKeyPress/useFKeyPress.tsx
+++ source/hooks/useFKeyPress/useFKeyPress.tsx
@@ -3,9 +3,12 @@ import useInputStatus from '../useInputStatus'
 
 const useFKeyPress = (callback: any) => {
   const isInputFocused = useInputStatus()
+  console.log(isInputFocused, '聚焦状态')
+
   React.useEffect(() => {
     const handleKeyPress = (event: any) => {
       if (event.key === 'f') {
+        event.preventDefault()
         if (!isInputFocused) {
           callback()
         }
