b427d78d24978459afe06f2d0be818b3cd307d90
文婷 <2547274792@qq.com>
2023-06-30 15:16:25 +0800

    style: 类别

--- source/components/CategoryDropdown/index.tsx
+++ source/components/CategoryDropdown/index.tsx
@@ -159,7 +159,9 @@ const CategoryDropdown = (props: IProps) => {
 
   useEffect(() => {
     if (categoryList) {
+      console.log(categoryList, '=categoryListcategoryList33333')
       const data = getTypeCategory(categoryList, 'work_type')
+      console.log(data, '=12121')
       const options = getOptions(data)
       if (!options) {
         return
