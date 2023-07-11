079d4e35dc02b6d54b5d3eb536fe58e28ae7616f
zhaochunmei <279658732@qq.com>
2023-07-11 18:04:07 +0800

    fix: 冲刺添加

--- source/components/IconFont.tsx
+++ source/components/IconFont.tsx
@@ -4,7 +4,7 @@ import { createFromIconfontCN } from '@ant-design/icons'
 
 const IconFont = createFromIconfontCN({
   scriptUrl: [
-    'https://lf1-cdn-tos.bytegoofy.com/obj/iconpark/svg_24216_175.6f7b10cec76ac6637b0ae895bd1e521c.js',
+    'https://lf1-cdn-tos.bytegoofy.com/obj/iconpark/svg_24216_176.95182352521a709a4b95d77753ca500a.js',
   ],
 })
 
