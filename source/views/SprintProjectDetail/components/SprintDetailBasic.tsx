eb467675086fbf346eb0c79545c900feeb85b0c6
文婷 <2547274792@qq.com>
2023-07-12 16:33:40 +0800

    style: 更新问题

--- source/views/SprintProjectDetail/components/SprintDetailBasic.tsx
+++ source/views/SprintProjectDetail/components/SprintDetailBasic.tsx
@@ -10,6 +10,7 @@ import { Tooltip } from 'antd'
 import { encryptPhp } from '@/tools/cryptoPhp'
 import { setActiveCategory } from '@store/category'
 import { useTranslation } from 'react-i18next'
+import { setIsUpdateChangeLog } from '@store/project'
 interface Props {
   onRef: any
 }
@@ -25,6 +26,7 @@ const SprintDetailBasic = (props: Props) => {
   // 更新详情
   const onUpdate = () => {
     dispatch(getAffairsInfo({ projectId: id, sprintId: affairsInfo.id || 0 }))
+    dispatch(setIsUpdateChangeLog(true))
   }
 
   // 跳转配置
