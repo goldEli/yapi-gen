04f3e1e706ef16d57727381f6f5c02fe11989beb
yangpeng <yangpeng@ifun.com>
2023-07-14 16:10:43 +0800

    fix: 看板拖拽取消后页面不刷新或卡死问题

--- source/views/KanBanBoard/IssueCard/index.tsx
+++ source/views/KanBanBoard/IssueCard/index.tsx
@@ -46,94 +46,100 @@ const IssueCard = (props: IssueCardProps) => {
   const { ids } = useStoryIds()
   const { projectInfo } = useSelector(store => store.project)
 
-  const cardContent = (dragOver: any) => (
-    <IssueCardBoxContainer isDragOver={dragOver} hidden={props.hidden}>
-      <Top>
-        <TopLeft>
-          <Tooltip title={item.project_category.name}>
-            <TopProjectIcon src={item.project_category.attachment_path} />
-          </Tooltip>
-          <TopText>{item.story_prefix_key}</TopText>
-        </TopLeft>
-        <TopRight>
-          <ThreeDot story={item} />
-        </TopRight>
-      </Top>
-      <Middle>
-        <StoryText
-          onClick={e => {
-            e.stopPropagation()
-            openDemandDetail(
-              { ...props.item, demandIds: ids, projectId: getProjectIdByUrl() },
-              getProjectIdByUrl(),
-              props.item.id,
-              1,
-            )
-          }}
-        >
-          {item.name}
-        </StoryText>
-      </Middle>
-      <div style={{ width: '100%', height: '22px', position: 'relative' }}>
-        <Bottom>
-          <BottomLeft>
-            <MultipleAvatar max={3} list={item.handlers} />
-          </BottomLeft>
-          <BottomRight>
-            {projectInfo.projectType === 1 && (
-              <PercentageBox>{`${item.schedule}%`}</PercentageBox>
-            )}
-            <Popover
-              content={
-                <PopoverTargetText>
-                  {item.iterate_info || '--'}
-                </PopoverTargetText>
-              }
-              placement="bottom"
-              trigger="click"
-            >
-              <Tooltip
-                title="冲刺目标"
-                placement="top"
-                trigger="click"
-                open={isOpen}
-                onOpenChange={e => setIsOpen(e)}
-              >
-                <WrapIcon
-                  type="target"
-                  style={{
-                    paddingTop: '3px',
-                  }}
-                />
-              </Tooltip>
-            </Popover>
-            <Sub
-              onClick={(e: any) => {
-                e.stopPropagation()
-                if (childRef.current) {
-                  childRef.current!.click()
+  const cardContent = (dragOver: any) => {
+    return (
+      <IssueCardBoxContainer isDragOver={dragOver} hidden={props.hidden}>
+        <Top>
+          <TopLeft>
+            <Tooltip title={item.project_category.name}>
+              <TopProjectIcon src={item.project_category.attachment_path} />
+            </Tooltip>
+            <TopText>{item.story_prefix_key}</TopText>
+          </TopLeft>
+          <TopRight>
+            <ThreeDot story={item} />
+          </TopRight>
+        </Top>
+        <Middle>
+          <StoryText
+            onClick={e => {
+              e.stopPropagation()
+              openDemandDetail(
+                {
+                  ...props.item,
+                  demandIds: ids,
+                  projectId: getProjectIdByUrl(),
+                },
+                getProjectIdByUrl(),
+                props.item.id,
+                1,
+              )
+            }}
+          >
+            {item.name}
+          </StoryText>
+        </Middle>
+        <div style={{ width: '100%', height: '22px', position: 'relative' }}>
+          <Bottom>
+            <BottomLeft>
+              <MultipleAvatar max={3} list={item.handlers} />
+            </BottomLeft>
+            <BottomRight>
+              {projectInfo.projectType === 1 && (
+                <PercentageBox>{`${item.schedule}%`}</PercentageBox>
+              )}
+              <Popover
+                content={
+                  <PopoverTargetText>
+                    {item.iterate_info || '--'}
+                  </PopoverTargetText>
                 }
-              }}
-            >
-              <WrapIcon type="apartment" />
-              <ChildDemandTable
-                ref={childRef}
-                value={item.children_count}
-                row={{ id: item.id }}
-              />
-            </Sub>
-            {item.story_config_priority.icon && (
-              <PriorityIcon
-                icon={item.story_config_priority.icon}
-                color={item.story_config_priority.color ?? ''}
-              />
-            )}
-          </BottomRight>
-        </Bottom>
-        {/* {isOpen && <TooltipText>{item.iterate_info || '--'}</TooltipText>} */}
-      </div>
-    </IssueCardBoxContainer>
-  )
+                placement="bottom"
+                trigger="click"
+              >
+                <Tooltip
+                  title="冲刺目标"
+                  placement="top"
+                  trigger="click"
+                  open={isOpen}
+                  onOpenChange={e => setIsOpen(e)}
+                >
+                  <WrapIcon
+                    type="target"
+                    style={{
+                      paddingTop: '3px',
+                    }}
+                  />
+                </Tooltip>
+              </Popover>
+              <Sub
+                onClick={(e: any) => {
+                  e.stopPropagation()
+                  if (childRef.current) {
+                    childRef.current!.click()
+                  }
+                }}
+              >
+                <WrapIcon type="apartment" />
+                <ChildDemandTable
+                  ref={childRef}
+                  value={item.children_count}
+                  row={{ id: item.id }}
+                />
+              </Sub>
+              {item.story_config_priority.icon && (
+                <PriorityIcon
+                  icon={item.story_config_priority.icon}
+                  color={item.story_config_priority.color ?? ''}
+                />
+              )}
+            </BottomRight>
+          </Bottom>
+          {/* {isOpen && <TooltipText>{item.iterate_info || '--'}</TooltipText>} */}
+        </div>
+      </IssueCardBoxContainer>
+    )
+  }
 
   return (
     <Draggable
