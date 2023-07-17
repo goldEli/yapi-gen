bc15d38569cd17c25c7df4fa1ca51a13ff15b4f5
杨一 <you@example.com>
2023-07-17 15:29:29 +0800

    fix: 消息延迟

--- source/views/SiteNotifications/components/SiteDrawer/SiteDrawer.tsx
+++ source/views/SiteNotifications/components/SiteDrawer/SiteDrawer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-promise-executor-return */
 /* eslint-disable require-atomic-updates */
 /* eslint-disable no-undefined */
 /* eslint-disable camelcase */
@@ -162,7 +163,7 @@ const SiteDrawer = () => {
 
   const setReads = async (values: any) => {
     const res = await setReadApi(values)
-
+    await new Promise(resolve => setTimeout(resolve, 2000))
     if (res.code === 0) {
       const res2 = await getContactStatistics()
       let num = 0
