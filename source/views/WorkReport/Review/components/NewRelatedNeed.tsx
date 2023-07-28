908a0bba9ef42233a7e3bef0e468a91218238644
yangpeng <yangpeng@ifun.com>
2023-07-28 18:26:03 +0800

    feat: 日报助手弹窗90%

---
+++ source/views/WorkReport/Review/components/NewRelatedNeed.tsx
@@ -0,0 +1,208 @@
+/* eslint-disable react/jsx-no-leaked-render */
+import CustomSelect from '@/components/CustomSelect'
+import IconFont from '@/components/IconFont'
+import { getMessage } from '@/components/Message'
+import { AddWrap } from '@/components/StyleCommon'
+import { getDemandList, getProjectList } from '@/services/daily'
+import styled from '@emotion/styled'
+import { Form } from 'antd'
+import { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+
+const WrapDiv = styled.div`
+  .ant-form-item-row {
+    display: flex;
+    flex-direction: row !important;
+  }
+  .ant-form-item-label {
+    padding: 0px !important;
+    padding-right: 12px !important;
+  }
+  .ant-form-item {
+    margin-bottom: 16px;
+  }
+  .item {
+    display: flex;
+    align-items: center;
+    .relative {
+      display: inline-block;
+      white-space: nowrap;
+      color: var(--primary-d2);
+      font-size: 14px;
+      font-family: SiYuanRegular;
+      margin: 0px 24px;
+      cursor: pointer;
+    }
+    .cancel {
+      display: inline-block;
+      white-space: nowrap;
+      font-size: 14px;
+      font-family: SiYuanRegular;
+      color: var(--neutral-n2);
+      cursor: pointer;
+    }
+  }
+`
+
+const ShowListWrap = styled.div`
+  padding: 8px 0px 0px 0px;
+  .li {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 14px;
+    font-family: SiYuanRegular;
+    color: var(--neutral-n2);
+    padding: 4px 8px;
+    cursor: pointer;
+    border-radius: 6px 6px 6px 6px;
+    .closeIcon {
+      display: none;
+    }
+    &:hover {
+      background-color: var(--neutral-n8);
+    }
+    &:hover .closeIcon {
+      display: inline-block;
+    }
+    .left {
+      display: inline-flex;
+      align-items: center;
+    }
+    .dot {
+      flex-shrink: 0;
+      display: inline-block;
+      width: 6px;
+      height: 6px;
+      background: var(--neutral-n2);
+      border-radius: 50%;
+      margin-right: 8px;
+    }
+  }
+`
+
+const NewRelatedNeed = (props: any) => {
+  const [lessForm] = Form.useForm()
+  const [t] = useTranslation()
+  const [chooseList, setChooseList] = useState<any>([])
+
+  const [show, setShow] = useState<boolean>(false)
+  const confirm = async () => {
+    const data = await lessForm.validateFields()
+    const newData = JSON.parse(JSON.stringify(data))
+    const result = newData.needs.map((i: any) => ({
+      ...i,
+    }))
+    const historyData = JSON.parse(JSON.stringify(chooseList))
+
+    props.onChange(historyData.concat(result).map((item: any) => item.value))
+    setChooseList(historyData.concat(result))
+
+    setShow(false)
+    lessForm.resetFields()
+    getMessage({ msg: t('p2.need3') as string, type: 'success' })
+  }
+
+  const del = (item: any) => {
+    const index = chooseList.findIndex((i: any) => i.key === item.key)
+    const newData = JSON.parse(JSON.stringify(chooseList))
+    newData.splice(index, 1)
+    setChooseList(newData)
+    props.onChange(newData.map((k: any) => k.value))
+  }
+
+  useEffect(() => {
+    setChooseList(
+      props?.initValue?.map((k: any) => ({
+        label: k.name,
+        key: k.id,
+        value: k.id,
+      })) ?? [],
+    )
+    props.onChange(props?.initValue?.map((item: any) => item.id))
+  }, [props.initValue])
+
+  return (
+    <div>
+      <AddWrap
+        onClick={() => {
+          setShow(true)
+        }}
+        hasColor
+      >
+        <IconFont type="plus" />
+        <div>{t('p2.RelatedRequirements')}</div>
+      </AddWrap>
+      <ShowListWrap>
+        {chooseList.map((item: any) => (
+          <div key={item.key} className="li">
+            <div className="left">
+              <span className="dot" />
+              <span>{item.label}</span>
+            </div>
+            <IconFont
+              className="closeIcon"
+              style={{ color: 'var(--neutral-n3)' }}
+              onClick={() => del(item)}
+              type="close"
+            />
+          </div>
+        ))}
+      </ShowListWrap>
+      {show && (
+        <WrapDiv>
+          <Form form={lessForm}>
+            <Form.Item
+              name="needs"
+              rules={[
+                {
+                  required: true,
+                  message: (
+                    <span
+                      style={{
+                        fontSize: '12px',
+                      }}
+                    >
+                      {t('p2.need1')}
+                    </span>
+                  ),
+                },
+              ]}
+            >
+              <div className="item">
+                <CustomSelect
+                  optionFilterProp="label"
+                  showSearch
+                  labelInValue
+                  mode="multiple"
+                  onChange={(val: any) => {
+                    lessForm.setFieldValue('needs', val)
+                  }}
+                  placeholder={t('common.pleaseSelect')}
+                  options={props?.data?.map((i: any) => ({
+                    label: i.name,
+                    value: i.id,
+                  }))}
+                />
+                <span className="relative" onClick={confirm}>
+                  关联
+                </span>
+                <span
+                  className="cancel"
+                  onClick={() => {
+                    setShow(false)
+                    lessForm.resetFields()
+                  }}
+                >
+                  {t('common.cancel')}
+                </span>
+              </div>
+            </Form.Item>
+          </Form>
+        </WrapDiv>
+      )}
+    </div>
+  )
+}
+
+export default NewRelatedNeed
--- source/views/WorkReport/Review/components/ReportAssistantModal.tsx
+++ source/views/WorkReport/Review/components/ReportAssistantModal.tsx
@@ -2,7 +2,7 @@ import CommonModal from '@/components/CommonModal'
 import NewLoadingTransition from '@/components/NewLoadingTransition'
 import styled from '@emotion/styled'
 import { Form, Spin } from 'antd'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { getMessage } from '@/components/Message'
 import CommonUserAvatar from '@/components/CommonUserAvatar'
 import NoPermissionModal from './NoPermissionModal'
@@ -12,6 +12,9 @@ import UploadAttach from '@/components/UploadAttach'
 import { AddWrap } from '@/components/StyleCommon'
 import IconFont from '@/components/IconFont'
 import CustomSelect from '@/components/CustomSelect'
+import NewRelatedNeed from './NewRelatedNeed'
+import { getDailyInfo, getProjectList, initDaily } from '@/services/report'
+import { getDemandList } from '@/services/daily'
 
 const HandleSpin = styled(Spin)`
   img {
@@ -63,7 +66,7 @@ const ContentWrap = styled.div`
     .ant-form-vertical .ant-form-item-label,
     .ant-col-24.ant-form-item-label,
     .ant-col-xl-24.ant-form-item-label {
-      padding: 0px;
+      padding: 0px 0px 8px 0px !important;
     }
     .project {
       display: flex;
@@ -123,6 +126,11 @@ const ReportAssistantModal = (props: ReportAssistantProps) => {
   const [havePermission, setHavePermission] = useState(false)
   const [form] = Form.useForm()
   const [t]: any = useTranslation()
+  const [initData, setInitData] = useState<any>(null)
+  const [currentProject, setCurrentProject] = useState<any>(null)
+  const [projectList, setProjectList] = useState<any>([])
+  const [demandList, setDemandList] = useState<any>([])
+  const [modalInfo, setModalInfo] = useState<any>(null)
   const [peopleValue, setPeopleValue] = useState<any>([])
   const [uploadAttachList, setUploadAttachList] = useState<any>({})
   const { close, visible } = props
@@ -130,6 +138,125 @@ const ReportAssistantModal = (props: ReportAssistantProps) => {
     getMessage({
       type: 'success',
       msg: '已通知管理员开启该功能',
+    })
+  }
+
+  const onClose = () => {
+    close()
+    setCurrentProject(null)
+  }
+
+  // 获取项目列表
+  const getProjectDataList = async () => {
+    const result = await getProjectList()
+    console.log(result, 'resultttt')
+    if (result) {
+      setProjectList(result)
+    }
+  }
+
+  const getDemandDataList = async (id: number) => {
+    const result = await getDemandList(id)
+    setDemandList(result?.data ?? [])
+  }
+
+  // 获取头部初始数据
+  const getInitDaily = async () => {
+    setInitData({
+      id: 689,
+      name: '蒋晓龙',
+      avatar: '',
+      company_name: '成都定星科技',
+      department_name: 'php',
+      job_name: 'php工程师',
+      daily_date: '2023-07-28',
+      report_title: '蒋晓龙的工作日报',
+    })
+    // const result = await initDaily()
+    // setInitData(result)
+  }
+
+  useEffect(() => {
+    getInitDaily()
+    getProjectDataList()
+  }, [])
+
+  useEffect(() => {
+    if (currentProject) {
+      getDemandDataList(currentProject.id)
+    }
+  }, [currentProject])
+
+  // 重新生成
+  const generatorDataByProject = async () => {
+    if (currentProject?.enable_hand_send === 2) {
+      setHavePermission(true)
+      return
+    }
+    const project = form.getFieldValue('project')
+    // const result = await getDailyInfo(project)
+    setModalInfo({
+      configs: [
+        {
+          id: 118,
+          name: '总进度',
+          report_template_id: 26,
+          type: 3,
+          tips: '',
+          is_required: 2,
+        },
+        {
+          id: 115,
+          name: '今日截止',
+          report_template_id: 26,
+          type: 4,
+          tips: '',
+          is_required: 1,
+          content: [],
+        },
+        {
+          id: 115,
+          name: '今日截止',
+          report_template_id: 26,
+          type: 2,
+          tips: '',
+          is_required: 1,
+          content: [],
+        },
+        {
+          id: 116,
+          name: '预期任务',
+          report_template_id: 26,
+          type: 4,
+          tips: '',
+          is_required: 1,
+          content: [
+            {
+              id: 1003095,
+              name: '需求测试xx',
+              story_prefix_key: 'CSXM（JXL-3',
+              project_id: 321,
+              is_end: 2,
+            },
+            {
+              id: 1002929,
+              name: '个人中心（jxl）',
+              story_prefix_key: 'CSXM（JXL-1',
+              project_id: 321,
+              is_end: 2,
+            },
+          ],
+        },
+        {
+          id: 114,
+          name: '汇报对象',
+          report_template_id: 26,
+          type: 1,
+          tips: '',
+          is_required: 1,
+        },
+      ],
+      reportUserList: [],
     })
   }
 
@@ -157,23 +284,6 @@ const ReportAssistantModal = (props: ReportAssistantProps) => {
           <Form.Item
             label={<LabelTitle>{content.name}</LabelTitle>}
             name={`${content.type}_${content.id}`}
-            rules={[
-              {
-                required: content.is_required === 1,
-                message: (
-                  <div
-                    style={{
-                      margin: '5px 0',
-                      fontSize: '12px',
-                      display: 'flex',
-                      alignItems: 'center',
-                    }}
-                  >
-                    {t('common.pleaseSelect')}
-                  </div>
-                ),
-              },
-            ]}
           >
             <ChoosePeople initValue={peopleValue} />
           </Form.Item>
@@ -185,7 +295,6 @@ const ReportAssistantModal = (props: ReportAssistantProps) => {
             name={`${content.type}_${content.id}`}
             rules={[
               {
-                required: content.is_required === 1,
                 message: (
                   <div
                     style={{
@@ -233,6 +342,20 @@ const ReportAssistantModal = (props: ReportAssistantProps) => {
             </div>
           </div>
         )
+      case 4:
+        return (
+          <Form.Item
+            label={
+              <LabelTitle>
+                {content.name}：{content?.content?.length}个
+              </LabelTitle>
+            }
+            name={`${content.type}_${content.id}`}
+            key={content.id}
+          >
+            <NewRelatedNeed initValue={content?.content} data={demandList} />
+          </Form.Item>
+        )
       default:
         return <div></div>
     }
@@ -244,7 +367,7 @@ const ReportAssistantModal = (props: ReportAssistantProps) => {
         width={784}
         title="日报助手"
         isVisible={visible}
-        onClose={close}
+        onClose={onClose}
         onConfirm={confirm}
         confirmText="发送"
       >
@@ -258,20 +381,32 @@ const ReportAssistantModal = (props: ReportAssistantProps) => {
           <HandleSpin spinning={false} indicator={<NewLoadingTransition />} />
           <ContentWrap>
             <div className="head">
-              <div className="tips">项目管理员已配置自动生成并发送规则</div>
+              {currentProject?.is_setting_config === 1 ? (
+                <div className="tips">项目管理员已配置自动生成并发送规则</div>
+              ) : null}
               <div className="userBox">
-                {false ? (
-                  <img className="avatar" src="" />
+                {initData?.avatar ? (
+                  <img className="avatar" src={initData?.avatar} />
                 ) : (
                   <CommonUserAvatar size="large" />
                 )}
                 <div className="desc">
                   <div className="title">
-                    李钟硕的工作日报
-                    <span className="date">（2022-08-21）</span>
+                    {initData?.report_title}
+                    <span className="date">
+                      {initData?.daily_date
+                        ? `（${initData?.daily_date}）`
+                        : ''}
+                    </span>
                   </div>
                   <div className="department">
-                    IFUN Games - 研发部 - 前端开发
+                    {[
+                      initData?.company_name,
+                      initData?.department_name,
+                      initData?.job_name,
+                    ]
+                      ?.filter((k: any) => k)
+                      ?.join('-')}
                   </div>
                 </div>
               </div>
@@ -283,10 +418,30 @@ const ReportAssistantModal = (props: ReportAssistantProps) => {
                   name="project"
                 >
                   <div className="project">
-                    <CustomSelect /> <AgainButton>重新生成</AgainButton>
+                    <CustomSelect
+                      optionFilterProp="label"
+                      showSearch
+                      onChange={(val: any) => {
+                        setCurrentProject(
+                          projectList?.find((item: any) => item.id === val),
+                        )
+                      }}
+                      options={projectList?.map((item: any) => ({
+                        label: item.name,
+                        value: item.id,
+                        key: item.id,
+                      }))}
+                    />
+                    <AgainButton onClick={generatorDataByProject}>
+                      重新生成
+                    </AgainButton>
                   </div>
                 </Form.Item>
-                <div>{generatorHtmlByData({ type: 3, name: '总体进度' })}</div>
+                {modalInfo?.configs?.map((item: any) => (
+                  <div style={{ marginBottom: 18 }} key={item.id}>
+                    {generatorHtmlByData(item)}
+                  </div>
+                ))}
               </Form>
             </div>
           </ContentWrap>
