f4d569c224cd2b04c20985caa6a892234271b435
zhaochunmei <279658732@qq.com>
2023-08-21 17:52:40 +0800

    fix: 时间

--- source/views/WorkingHoursStatistics/index.tsx
+++ source/views/WorkingHoursStatistics/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import ProjectCommonOperation from '@/components/CommonProjectComponent/CommonHeader'
 import WorkHoursPanel from './components/WorkHoursPanel'
@@ -9,7 +9,10 @@ import TableLeft from './components/TableLeft'
 interface IProps {}
 const WorkHours: React.FC<IProps> = props => {
   const [t] = useTranslation()
-  const onInputSearch = () => {}
+  const [formVal, setFormVal] = useState<any>()
+  const onInputSearch = (val: any) => {
+    console.log(val)
+  }
   return (
     <WorkHoursWrap>
       <ProjectCommonOperation
@@ -17,7 +20,11 @@ const WorkHours: React.FC<IProps> = props => {
         title={t('search_for_transaction_name_or_number')}
       />
 
-      <WorkHoursHeader />
+      <WorkHoursHeader
+        onSearch={(val: any) => {
+          setFormVal(val), console.log(val)
+        }}
+      />
       <MianWrap>
         <TableLeft />
         <WorkHoursPanel />
