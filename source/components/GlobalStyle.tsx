99a1087c63848a19f79f228c953b5f19060e00b0
杨一 <you@example.com>
2023-09-18 18:07:32 +0800

    fix: 优化样式

--- source/components/GlobalStyle.tsx
+++ source/components/GlobalStyle.tsx
@@ -54,8 +54,8 @@ const GlobalStyle = () => {
       --neutral-n3: #969799;
       --neutral-n4: #bbbdbf;
       --neutral-n5: #d5d6d9;
-      --neutral-n6-d1: #ecedef;
-      --neutral-n6-d2: #ecedef;
+      --neutral-n6-d1: #ebeced;
+      --neutral-n6-d2: #ebeced;
       --neutral-n7: #f2f2f4;
       --neutral-n8: #f5f5f7;
       --neutral-n9: #f9f9fa;
