967f4fefafcbef8d59a3eaa9facc71d12aafcccb
文婷 <2547274792@qq.com>
2023-09-25 14:15:37 +0800

    style: 检查看板改为Kanban

--- source/views/KanBanBoard/KanBanBtnsArea/index.tsx
+++ source/views/KanBanBoard/KanBanBtnsArea/index.tsx
@@ -1,19 +1,16 @@
 // 需求主页右侧操作组件
 /* eslint-disable complexity */
 /* eslint-disable @typescript-eslint/naming-convention */
-import { Space, Menu, message } from 'antd'
+import { Space } from 'antd'
 import styled from '@emotion/styled'
-import { getIsPermission, getParamsData } from '@/tools/index'
 import { useTranslation } from 'react-i18next'
 import { useMemo, useState } from 'react'
 import { useDispatch, useSelector } from '@store/index'
-import { useLocation, useSearchParams } from 'react-router-dom'
+import { useSearchParams } from 'react-router-dom'
 import SetShowField from './SetShowField'
 import SaveAsViewModal from './SaveAsViewModal'
-import { DividerWrap, HasIconMenu, HoverWrap } from '@/components/StyleCommon'
-import { getMessage } from '@/components/Message'
+import { DividerWrap } from '@/components/StyleCommon'
 import ScreenMinHover from '@/components/ScreenMinHover'
-import IconFont from '@/components/IconFont'
 import DropDownMenu from '@/components/DropDownMenu'
 import useShareModal from '@/hooks/useShareModal'
 import { onFullScreenMode } from '@store/kanBan/kanBan.thunk'
@@ -39,10 +36,7 @@ const SpaceWrap = styled(Space)({
 
 const KanBanBtnsArea = (props: Props) => {
   const [t] = useTranslation()
-  const [searchParams] = useSearchParams()
-  const paramsData = getParamsData(searchParams)
   const { projectInfo } = useSelector(store => store.project)
-  const [isVisible, setIsVisible] = useState(false)
   const [isVisibleFields, setIsVisibleFields] = useState(false)
   const dispatch = useDispatch()
   const { userInfo } = useSelector(store => store.user)
@@ -64,94 +58,7 @@ const KanBanBtnsArea = (props: Props) => {
     return key
   }, [sortByGroupOptions])
 
-  const hasFilter = getIsPermission(
-    projectInfo?.projectPermissions,
-    projectInfo.projectType === 1 ? 'b/story/get' : 'b/transaction/get',
-  )
-
   const { open, ShareModal } = useShareModal()
-
-  // 切换显示类型
-  const onClickMenu = (number: any) => {
-    getMessage({ msg: t('version2.reviewModeChangeSuccess'), type: 'success' })
-    props.onChangeGrid?.(number)
-    setIsVisible(false)
-  }
-
-  const menuType = () => {
-    let menuItems = [
-      {
-        key: 'list',
-        label: (
-          <HasIconMenu onClick={() => onClickMenu(0)} isCheck={!props.isGrid}>
-            <div className="left">
-              <IconFont className="icon" type="unorderedlist" />
-              <span
-                style={{
-                  color: 'var(--neutral-n2)',
-                }}
-                className="label"
-              >
-                {t('common.list')}
-              </span>
-            </div>
-            <IconFont className="checked" type={props.isGrid ? '' : 'check'} />
-          </HasIconMenu>
-        ),
-      },
-      {
-        key: 'thumbnail',
-        label: (
-          <HasIconMenu
-            onClick={() => onClickMenu(1)}
-            isCheck={props.isGrid === 1}
-          >
-            <div className="left">
-              <IconFont className="icon" type="layout" />
-              <span
-                style={{
-                  color: 'var(--neutral-n2)',
-                }}
-                className="label"
-              >
-                {t('common.board')}
-              </span>
-            </div>
-            <IconFont
-              className="checked"
-              type={props.isGrid === 1 ? 'check' : ''}
-            />
-          </HasIconMenu>
-        ),
-      },
-      {
-        key: 'tree',
-        label: (
-          <HasIconMenu
-            onClick={() => onClickMenu(2)}
-            isCheck={props.isGrid === 2}
-          >
-            <div className="left">
-              <IconFont className="icon" type="tree-list" />
-              <span
-                style={{
-                  color: 'var(--neutral-n2)',
-                }}
-                className="label"
-              >
-                {t('version2.tree')}
-              </span>
-            </div>
-            <IconFont
-              className="checked"
-              type={props.isGrid === 2 ? 'check' : ''}
-            />
-          </HasIconMenu>
-        ),
-      },
-    ]
-    return <Menu items={menuItems} />
-  }
 
   return (
     <SpaceWrap size={8} style={{ marginLeft: 8 }}>
@@ -182,33 +89,6 @@ const KanBanBtnsArea = (props: Props) => {
         }}
       />
       <DividerWrap type="vertical" />
-      {/* 视图 */}
-      {/* <>
-        <ViewPort pid={projectId} type={2} />
-        <DividerWrap type="vertical" />
-      </> */}
-
-      {/* <DropDownMenu
-        isVisible={isVisible}
-        onChangeVisible={setIsVisible}
-        menu={menuType}
-        icon={
-          props.isGrid === 1
-            ? 'layout'
-            : props.isGrid === 2
-            ? 'tree-list'
-            : 'unorderedlist'
-        }
-      >
-        <HasIconMenu>
-          <div className="label">
-            {props.isGrid === 1 && t('common.board')}
-            {props.isGrid === 2 && t('version2.tree')}
-            {!props.isGrid && t('common.list')}
-          </div>
-        </HasIconMenu>
-      </DropDownMenu> */}
-
       {/* 筛选 */}
       <>
         <ScreenMinHover
--- source/views/ProjectSetting/components/KanBanSetting/KanBanBtnsArea/index.tsx
+++ source/views/ProjectSetting/components/KanBanSetting/KanBanBtnsArea/index.tsx
@@ -1,20 +1,16 @@
 // 需求主页右侧操作组件
 /* eslint-disable complexity */
 /* eslint-disable @typescript-eslint/naming-convention */
-import { Space, Menu, message } from 'antd'
+import { Space } from 'antd'
 import styled from '@emotion/styled'
-import { getIsPermission, getParamsData } from '@/tools/index'
 import { useTranslation } from 'react-i18next'
 import { useState } from 'react'
-import { useDispatch, useSelector } from '@store/index'
-import { useLocation, useSearchParams } from 'react-router-dom'
+import { useDispatch } from '@store/index'
 import SetShowField from './SetShowField'
 import SaveAsViewModal from './SaveAsViewModal'
 import ShareModal from './ShareModal'
-import { DividerWrap, HasIconMenu, HoverWrap } from '@/components/StyleCommon'
-import { getMessage } from '@/components/Message'
+import { DividerWrap } from '@/components/StyleCommon'
 import ScreenMinHover from '@/components/ScreenMinHover'
-import IconFont from '@/components/IconFont'
 import DropDownMenu from '@/components/DropDownMenu'
 import { openShareModel } from '@store/kanBan/kanBan.thunk'
 
@@ -38,101 +34,9 @@ const SpaceWrap = styled(Space)({
 
 const KanBanBtnsArea = (props: Props) => {
   const [t] = useTranslation()
-  const location = useLocation()
-  const [searchParams] = useSearchParams()
-  const paramsData = getParamsData(searchParams)
-  const projectId = paramsData.id
-  const { projectInfo } = useSelector(store => store.project)
-  const [isVisible, setIsVisible] = useState(false)
   const [isVisibleFields, setIsVisibleFields] = useState(false)
   const dispatch = useDispatch()
 
-  const hasFilter = getIsPermission(
-    projectInfo?.projectPermissions,
-    projectInfo.projectType === 1 ? 'b/story/get' : 'b/transaction/get',
-  )
-
-  // 切换显示类型
-  const onClickMenu = (number: any) => {
-    getMessage({ msg: t('version2.reviewModeChangeSuccess'), type: 'success' })
-    props.onChangeGrid?.(number)
-    setIsVisible(false)
-  }
-
-  const menuType = () => {
-    let menuItems = [
-      {
-        key: 'list',
-        label: (
-          <HasIconMenu onClick={() => onClickMenu(0)} isCheck={!props.isGrid}>
-            <div className="left">
-              <IconFont className="icon" type="unorderedlist" />
-              <span
-                style={{
-                  color: 'var(--neutral-n2)',
-                }}
-                className="label"
-              >
-                {t('common.list')}
-              </span>
-            </div>
-            <IconFont className="checked" type={props.isGrid ? '' : 'check'} />
-          </HasIconMenu>
-        ),
-      },
-      {
-        key: 'thumbnail',
-        label: (
-          <HasIconMenu
-            onClick={() => onClickMenu(1)}
-            isCheck={props.isGrid === 1}
-          >
-            <div className="left">
-              <IconFont className="icon" type="layout" />
-              <span
-                style={{
-                  color: 'var(--neutral-n2)',
-                }}
-                className="label"
-              >
-                {t('common.board')}
-              </span>
-            </div>
-            <IconFont
-              className="checked"
-              type={props.isGrid === 1 ? 'check' : ''}
-            />
-          </HasIconMenu>
-        ),
-      },
-      {
-        key: 'tree',
-        label: (
-          <HasIconMenu
-            onClick={() => onClickMenu(2)}
-            isCheck={props.isGrid === 2}
-          >
-            <div className="left">
-              <IconFont className="icon" type="tree-list" />
-              <span
-                style={{
-                  color: 'var(--neutral-n2)',
-                }}
-                className="label"
-              >
-                {t('version2.tree')}
-              </span>
-            </div>
-            <IconFont
-              className="checked"
-              type={props.isGrid === 2 ? 'check' : ''}
-            />
-          </HasIconMenu>
-        ),
-      },
-    ]
-    return <Menu items={menuItems} />
-  }
   return (
     <SpaceWrap size={8} style={{ marginLeft: 8 }}>
       {/* 分享 */}
@@ -144,32 +48,6 @@ const KanBanBtnsArea = (props: Props) => {
         }}
       />
       <DividerWrap type="vertical" />
-      {/* 视图 */}
-      {/* <>
-        <ViewPort pid={projectId} type={2} />
-        <DividerWrap type="vertical" />
-      </> */}
-
-      {/* <DropDownMenu
-        isVisible={isVisible}
-        onChangeVisible={setIsVisible}
-        menu={menuType}
-        icon={
-          props.isGrid === 1
-            ? 'layout'
-            : props.isGrid === 2
-            ? 'tree-list'
-            : 'unorderedlist'
-        }
-      >
-        <HasIconMenu>
-          <div className="label">
-            {props.isGrid === 1 && t('common.board')}
-            {props.isGrid === 2 && t('version2.tree')}
-            {!props.isGrid && t('common.list')}
-          </div>
-        </HasIconMenu>
-      </DropDownMenu> */}
 
       {/* 筛选 */}
       <>
