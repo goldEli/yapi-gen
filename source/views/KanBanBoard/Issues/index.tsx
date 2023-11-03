dd5233be4c7464562ab1d979ccdb32a3b7bbff57
yangyi <627814058@qq.com>
2023-11-03 17:48:21 +0800

    fix: 高度控制

--- source/views/KanBanBoard/Issues/index.tsx
+++ source/views/KanBanBoard/Issues/index.tsx
@@ -172,9 +172,9 @@ const Issues: React.FC<IssuesProps> = props => {
       style={{
         overflowY: 'auto',
         overflowX: 'hidden',
-        height: '500px',
+        height: groupType === 'none' ? 'calc( 100vh - 300px)' : '500px',
       }}
-      height="500px"
+      height={groupType === 'none' ? 'calc( 100vh - 300px)' : '500px'}
       dataLength={issues.stories?.length}
       next={fetchMoreData}
       hasMore
