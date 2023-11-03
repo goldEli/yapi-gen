e6b1d0836d49b667ad5fa6118e3d4e474bd2e647
yangyi <627814058@qq.com>
2023-11-03 16:10:34 +0800

    fix: 看板改需求拖拽

--- source/views/KanBanBoard/hooks/useKanBanData/index.tsx
+++ source/views/KanBanBoard/hooks/useKanBanData/index.tsx
@@ -114,45 +114,6 @@ const useKanBanData = () => {
         )
       }
     }
-
-    // // 跨容器拖动
-    // if (source.droppableId !== destination.droppableId) {
-    //   setData(
-    //     produce(draft => {
-    //       // 获取拖动源数据
-    //       const sourceData = draft
-    //         .find(item => item.groupId === getId(source.droppableId).groupId)
-    //         ?.data.find(item => item.id === getId(source.droppableId).id)
-    //       // 获取目标数据
-    //       const destinationData = draft
-    //         .find(
-    //           item => item.groupId === getId(destination.droppableId).groupId,
-    //         )
-    //         ?.data.find(item => item.id === getId(destination.droppableId).id)
-    //       // 源移除的卡片数据
-    //       const [removed] = sourceData?.list?.splice(source.index, 1) ?? []
-    //       // 移除的卡片数据插入目标中
-    //       if (removed) {
-    //         destinationData?.list?.splice(destination.index, 0, removed)
-    //       }
-    //     }),
-    //   )
-    //   return
-    // }
-    // setData(
-    //   produce(draft => {
-    //     // 获取拖动源数据
-    //     const sourceData = draft
-    //       .find(item => item.groupId === getId(source.droppableId).groupId)
-    //       ?.data.find(item => item.id === getId(source.droppableId).id)
-    //     // 源移除的卡片数据
-    //     const [removed] = sourceData?.list?.splice(source.index, 1) ?? []
-    //     // 移除的卡片数据插入目标中
-    //     if (removed) {
-    //       sourceData?.list?.splice(destination.index, 0, removed)
-    //     }
-    //   }),
-    // )
   }
 
   return {
