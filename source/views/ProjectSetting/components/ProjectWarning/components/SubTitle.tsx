24a1a4a876c84c0105791563e9deb10c4d6da27a
xuewanyun <xuewanyun@ifun.com>
2023-11-07 18:26:04 +0800

    feat: 标题组件

--- source/views/ProjectSetting/components/ProjectWarning/components/SubTitle.tsx
+++ source/views/ProjectSetting/components/ProjectWarning/components/SubTitle.tsx
@@ -1,6 +1,27 @@
+import styled from '@emotion/styled'
 import React from 'react'
-import { SubTitleBox } from '../style'
-
+const SubTitleBox = styled.div`
+  position: relative;
+  padding-left: 12px;
+  display: flex;
+  align-items: center;
+  height: 22px;
+  color: var(--neutral-n1-d1);
+  font-size: var(--font14);
+  font-family: SiYuanMedium;
+  margin: 32px 0px 16px 0px;
+  &::before {
+    position: absolute;
+    content: '';
+    left: 0px;
+    top: 2px;
+    width: 3px;
+    height: 16px;
+    background: var(--primary-d1);
+    display: flex;
+    align-items: center;
+  }
+`
 const SubTitle = (props: any) => {
   const { title } = props
   return <SubTitleBox>{title}</SubTitleBox>
