69da9edcd9f6a0a95106f88b66ece250247e2ca0
xuewanyun <xuewanyun@ifun.com>
2023-11-10 16:18:05 +0800

    feat: 解决bug

--- source/views/ProjectSetting/components/ProjectWarning/components/WaringCard.tsx
+++ source/views/ProjectSetting/components/ProjectWarning/components/WaringCard.tsx
@@ -73,15 +73,17 @@ const WaringCard = (props: WaringCardProps) => {
     })
   }
   const noticeTypeLabel = () => {
-    return push_channel?.map((item: any, index: number) => {
-      return (
-        <span key={index}>
-          {noticeType[item.type]}
-          {index !== push_channel?.length - 1 &&
-            (language === 'zh' ? <i>、</i> : <i> / </i>)}
-        </span>
-      )
-    })
+    return push_channel
+      ?.filter((item: any) => item.is_enable === 1)
+      .map((item: any, index: number) => {
+        return (
+          <span key={index}>
+            {noticeType[item.type]}
+            {index !== push_channel?.length - 1 &&
+              (language === 'zh' ? <i>、</i> : <i> / </i>)}
+          </span>
+        )
+      })
   }
 
   return (
@@ -154,16 +156,24 @@ const WaringCard = (props: WaringCardProps) => {
         <WaringCardItem style={{ width: '45%', marginTop: 16 }}>
           <div className="label">{t('pushConditions')}：</div>
           <div className="content">
-            {push_condition?.map((item: any) => (
-              <span key={item.type}>
-                [{taskType[item.type]}] {item.cond_conf}
-                {item.type === 'bug_too_many'
-                  ? t('moreThanThan1')
-                  : t('moreThanThan')}
-                {item.send_conf}
-                {t('strip')}
-              </span>
-            ))}
+            {push_condition
+              ?.filter((item: any) => item.is_enable === 1)
+              .map((item: any) => (
+                <span key={item.type}>
+                  [{taskType[item.type]}]
+                  {item.type === 'bug_too_many'
+                    ? t('moreThanThan1', {
+                        day: item.cond_conf,
+                        count: item.send_conf,
+                      })
+                    : t('moreThanThan', {
+                        day: item.cond_conf,
+                        count: item.send_conf,
+                      })}
+                  {/* {item.send_conf} */}
+                  {t('strip')}
+                </span>
+              ))}
           </div>
         </WaringCardItem>
         <WaringCardItem style={{ width: '40%', marginTop: 16 }}>
