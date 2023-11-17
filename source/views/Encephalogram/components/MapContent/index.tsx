905b802834f6d2d57ccb0f25abc1d8df4d773d0f
yangpeng <yangpeng@ifun.com>
2023-11-17 18:46:30 +0800

    feat: 基本页面搭建2

--- source/views/Encephalogram/components/MapContent/index.tsx
+++ source/views/Encephalogram/components/MapContent/index.tsx
@@ -9,48 +9,136 @@ const MapContent = (props: any) => {
     const container = document.querySelector('#MapContentMountNode')
     if (container) {
       G6.registerNode(
-        'dice-mind-map-sub',
+        'customNode',
         {
-          jsx: (cfg: any) => {
-            const width = Util.getTextSize(cfg.label, 14)[0] + 16
-            const color = cfg.color || cfg.style.stroke
-
-            return `
-                <group>
-                  <rect draggable="true" style={{width: ${
-                    width + 16
-                  }, height: 22}} keyshape>
-                    <text draggable="true" style={{ fontSize: 14, marginLeft: 12, marginTop: 6 }}>${
-                      cfg.label
-                    }</text>
-                  </rect>
-                  <rect style={{ fill: ${color}, width: ${
-              width + 16
-            }, height: 2, x: 0, y: 22 }} />
-                  
-                </group>
-              `
-          },
+          // jsx: (cfg: any) => {
+          //   const width = Util.getTextSize(cfg.label, 14)[0] + 16
+          //   const color = cfg.color || cfg.style.stroke
+          //   return `
+          //       <group>
+          //         <rect draggable="true" style={{width: ${
+          //           width + 16
+          //         }, height: 40,fill:'#FFF383',textAlign: 'center', }} keyshape>
+          //           <text draggable="true" style={{ fontSize: 14,marginLeft:16 ,marginTop: 10}}>${
+          //             cfg.label
+          //           }</text>
+          //         </rect>
+          //       </group>
+          //     `
+          // },
           getAnchorPoints() {
             return [
-              [0, 0.965],
-              [1, 0.965],
+              [0, 0.5],
+              [1, 0.5],
             ]
+          },
+          draw: (cfg: any, group: any) => {
+            console.log(cfg, group, 'xxxxxx')
+            const content = cfg.label
+            const { fill, color, fontSize, fontWeight, fontFamily } =
+              cfg.style || {}
+            const rect = group.addShape('rect', {
+              attrs: {
+                fill,
+                stroke: '#D5D6D9',
+                width: Util.getTextSize(cfg.label, fontSize || 14)[0] + 20,
+                height: 40,
+              },
+              name: 'rect-shape',
+            })
+            const bbox = rect.getBBox()
+            group.addShape('text', {
+              attrs: {
+                text: content,
+                x: 10,
+                y: bbox.height / 2,
+                textBaseline: 'middle',
+                fill: color || '#323233',
+                fontWeight,
+                fontSize: fontSize || 14,
+                fontFamily: 'SiYuanMedium',
+              },
+              name: 'text-shape',
+            })
+
+            const hasChildren = cfg.children && cfg.children.length > 0
+            if (hasChildren) {
+              group.addShape('marker', {
+                attrs: {
+                  x: bbox.width - 1.5,
+                  y: bbox.height / 2,
+                  r: 6,
+                  symbol: cfg.collapsed ? G6.Marker.expand : G6.Marker.collapse,
+                  stroke: '#969799',
+                  lineWidth: 1,
+                },
+                name: 'collapse-icon',
+              })
+            }
+            return group
+          },
+          update: (cfg, item) => {
+            const group = item.getContainer()
+            const icon = group.find(e => e.get('name') === 'collapse-icon')
+            icon.attr(
+              'symbol',
+              cfg.collapsed ? G6.Marker.expand : G6.Marker.collapse,
+            )
+          },
+          setState(name, value, item: any) {
+            const keyShape = item.getKeyShape()
+            switch (name) {
+              case 'hover':
+                keyShape.attr({
+                  stroke: '#f00',
+                  fill: '#f00',
+                })
+                break
+            }
           },
         },
         'rect',
       )
       const tooltip = new G6.Tooltip({
-        getContent(e) {
-          return `<div style='width: 180px;'>
-                <ul id='menu' style='cursor:pointer;'>
-                  <li title='1'>测试02</li>
-                  <li title='2'>测试02</li>
-                  <li>测试02</li>
-                  <li>测试02</li>
-                  <li>测试02</li>
-                </ul>
-              </div>`
+        offsetX: 10,
+        offsetY: 10,
+        itemTypes: ['node'],
+        // 自定义 tooltip 内容
+        getContent: (e: any) => {
+          const outDiv = document.createElement('div')
+          outDiv.style.width = 'fit-content'
+          outDiv.innerHTML = `
+          <ul style="color: #ffffff;list-style: none; margin: 0px;padding:0px">
+          <li style="margin-bottom: 8px">预计工期：25 天</li>
+          <li style="margin-bottom: 8px">实际工期：24天</li>
+          <li style="margin-bottom: 8px">当前进度：30%</li>
+          <li style="display: flex">
+            <div style="white-space: nowrap; margin-right: 8px">处理人：</div>
+            <div style="display: flex; flex-direction: column">
+              <div style="display: flex; font-size: 12px; margin-bottom: 8px;align-items:center">
+                <img
+                  src="https://oa-1308485183.cos.ap-chengdu.myqcloud.com/oa-dev-img/1504303190303051778/1551758466375991298/2023-04-27/studios_3.webp"
+                  width="24px"
+                  height="24px"
+                  style="margin-right: 4px;border-radius: 50%"
+                />
+                美术组-UI组长-张三
+              </div>
+              <div style="display: flex; font-size: 12px;align-items:center">
+                <img
+                  src="https://oa-1308485183.cos.ap-chengdu.myqcloud.com/oa-dev-img/1504303190303051778/1551758466375991298/2023-04-27/studios_3.webp"
+                  width="24px"
+                  height="24px"
+                  style="margin-right: 4px;border-radius: 50%"
+                />
+                软件大数据组-UI组长-张三
+              </div>
+            </div>
+          </li>
+        </ul>
+            `
+          return outDiv
+          // <li>预计工期: ${e.item.getModel().label || e.item.getModel().id}</li>
         },
       })
       const width = container.scrollWidth
@@ -78,13 +166,12 @@ const MapContent = (props: any) => {
           ],
         },
         defaultNode: {
-          type: 'dice-mind-map-sub',
-          size: [100, 40],
+          type: 'customNode',
         },
         defaultEdge: {
           type: 'polyline',
           style: {
-            stroke: '#A3B1BF',
+            stroke: '#D5D6D9',
           },
         },
         layout: {
@@ -97,41 +184,227 @@ const MapContent = (props: any) => {
         plugins: [tooltip],
       })
       const data = {
-        label: 'Modeling Methods',
+        id: '1',
+        label: 'SLG框架开发',
+        style: {
+          color: '#323233',
+          fontWeight: 500,
+          fontFamily: 'SiYuanMedium',
+          fontSize: 18,
+          fill: '#FFFFFF',
+        },
         children: [
           {
-            label: 'Classification',
+            id: '11',
+            label: '主分类-英雄',
+            style: {
+              color: '#323233',
+              fontWeight: 500,
+              fontFamily: 'SiYuanMedium',
+              fontSize: 16,
+              fill: '#FFFFFF',
+            },
             children: [
               {
-                label: 'Logistic regression',
+                id: '111',
+                label: '子分类-等级',
                 style: {
-                  fill: '#bae637',
-                  lineWidth: 5,
+                  fill: '#FFFFFF',
                 },
+                children: [
+                  {
+                    id: '111-1',
+                    label: '父级任务-英雄养成',
+                    style: {
+                      fill: '#FFF383',
+                    },
+                    children: [
+                      {
+                        id: '1111-1',
+                        label: '子任务-UE 2.0',
+                        style: {
+                          fill: '#BBFFBA',
+                        },
+                        children: [
+                          {
+                            id: '11111-1',
+                            label: '美术组UED1',
+                          },
+                        ],
+                      },
+                      {
+                        id: '1111-2',
+                        label: '子任务-UE 2.1',
+                        style: {
+                          fill: '#BBFFBA',
+                        },
+                        children: [
+                          {
+                            id: '1111-2-1',
+                            label: '美术组UED2',
+                          },
+                        ],
+                      },
+                      {
+                        id: '1111-3',
+                        label: '子任务-UE 2.2',
+                        style: {
+                          fill: '#E4D8FF',
+                        },
+                        children: [
+                          {
+                            id: '1111-3-1',
+                            label: '美术组UED3',
+                          },
+                        ],
+                      },
+                      {
+                        id: '1111-4',
+                        label: '子任务-UE 2.3',
+                        style: {
+                          fill: '#FFC8A0',
+                        },
+                        children: [
+                          {
+                            id: '1111-4-1',
+                            label: '3D设计组',
+                          },
+                        ],
+                      },
+                      {
+                        id: '1111-5',
+                        label: '子任务-UE 2.4',
+                        style: {
+                          fill: '#E4D8FF',
+                        },
+                        children: [
+                          {
+                            id: '1111-5-1',
+                            label: '3D设计组',
+                          },
+                        ],
+                      },
+                      {
+                        id: '1111-6',
+                        label: '子任务-UE 2.5',
+                        style: {
+                          fill: '#FFC8A0',
+                        },
+                        children: [
+                          {
+                            id: '1111-6-1',
+                            label: '客户端组',
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                  {
+                    id: '111-2',
+                    label: '父级任务-英雄等级',
+                    style: {
+                      fill: '#BBFFBA',
+                    },
+                  },
+                  {
+                    id: '111-3',
+                    label: '父级任务-英雄主界面',
+                    style: {
+                      fill: '#FFC8A0',
+                    },
+                  },
+                ],
               },
-              { label: 'Linear discriminant analysis' },
-              { label: 'Probabilistic neural network' },
-              { label: 'Probabilistic neural network11' },
-              { label: 'Support vector machine' },
+              {
+                id: '112',
+                label: '子分类-天赋',
+                style: {
+                  fill: '#FFFFFF',
+                },
+                children: [
+                  {
+                    id: '112-1',
+                    label: '父级任务-天赋-ui2.0',
+                    style: {
+                      fill: '#FFF383',
+                    },
+                  },
+                  {
+                    id: '112-2',
+                    label: '父级任务-天赋-ue2.0',
+                    style: {
+                      fill: '#BBFFBA',
+                    },
+                  },
+                ],
+              },
+              {
+                id: '113',
+                label: '子分类-星际',
+                style: {
+                  fill: '#FFFFFF',
+                },
+                children: [
+                  {
+                    id: '113-1',
+                    label: '父级任务-天赋-ui2.0',
+                    style: {
+                      fill: '#BBFFBA',
+                    },
+                  },
+                  {
+                    id: '113-2',
+                    label: '父级任务-天赋-ue2.0',
+                    style: {
+                      fill: '#E4D8FF',
+                    },
+                  },
+                ],
+              },
             ],
           },
           {
-            label: 'Regression',
-            children: [
-              { label: 'Multiple linear regression' },
-              { label: 'Partial least squares' },
-              { label: 'Multi-layer feedforward neural network' },
-              { label: 'General regression neural network' },
-              { label: 'Support vector regression' },
-              { label: 'Support vector regression2' },
-              { label: 'Support vector regression3' },
-            ],
+            id: '22',
+            label: '副分类-练级',
+            style: {
+              color: '#323233',
+              fontWeight: 500,
+              fontFamily: 'SiYuanMedium',
+              fontSize: 16,
+              fill: '#FFFFFF',
+            },
           },
         ],
       }
       graph.data(data)
       graph.render()
-      graph.fitView()
+      graph.fitCenter()
+      graph.on('node:mouseenter', (e: any) => {
+        graph.setItemState(e.item, 'hover', true)
+      })
+
+      graph.on('node:mouseleave', (e: any) => {
+        graph.setItemState(e.item, 'hover', false)
+      })
+
+      // graph.on('node:mouseenter', (e: any) => {
+      //   const node = e.item
+      //   graph.updateItem(node, {
+      //     style: {
+      //       stroke: 'rgba(143, 188, 255, 0.7)',
+      //       lineWidth: 3,
+      //     },
+      //   })
+      // })
+      // graph.on('node:mouseleave', (e: any) => {
+      //   const node = e.item
+      //   graph.updateItem(node, {
+      //     style: {
+      //       stroke: '#D5D6D9',
+      //       lineWidth: 3,
+      //     },
+      //   })
+      // })
     }
   }, [])
 
--- source/views/Encephalogram/styles.tsx
+++ source/views/Encephalogram/styles.tsx
@@ -4,12 +4,25 @@ export const EncephalogramBox = styled.div`
   width: 100%;
   height: 100%;
   position: relative;
+  cursor: pointer;
+  padding: 26px;
+  box-sizing: border-box;
+  .g6-component-tooltip {
+    background-color: rgba(0, 0, 0, 0.7);
+    border-radius: 6px;
+  }
 `
 
 export const MapContentBox = styled.div`
+  canvas {
+    cursor: pointer !important;
+  }
   width: 100%;
   height: 100%;
-  border: 1px solid red;
+  cursor: pointer;
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-image: url('https://mj-system-1308485183.cos.ap-chengdu.myqcloud.com/public/mind/dotBg.png');
 `
 export const TopAreaBox = styled.div`
   position: absolute;
