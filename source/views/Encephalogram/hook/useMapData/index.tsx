c7915316ba273f1007cdade19c70d986d5bd3ccc
yangpeng <yangpeng@ifun.com>
2023-11-21 11:29:43 +0800

    feat: hover节点边框颜色改变

--- source/views/Encephalogram/hook/useMapData/index.tsx
+++ source/views/Encephalogram/hook/useMapData/index.tsx
@@ -11,54 +11,6 @@ const useMapData = () => {
     }
     return []
   }, [projectId])
-
-  const dd = {
-    id: 321,
-    name: '测试项目（JXL）',
-    children: [
-      {
-        id: 516,
-        name: '分类1',
-        parent_id: 0,
-        children: [
-          {
-            id: 1002929,
-            name: '个人中心（jxl）',
-            parent_id: 516,
-            children: [
-              {
-                id: 1003712,
-                name: '测试关联工作项',
-                parent_id: 1002929,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  }
-
-  const arr = [
-    { id: 321, name: '测试项目（JXL）' },
-    {
-      id: 516,
-      name: '分类1',
-      parent_id: 0,
-    },
-    {
-      id: 1002929,
-      name: '个人中心（jxl）',
-      parent_id: 516,
-    },
-    {
-      id: 1003712,
-      name: '测试关联工作项',
-      parent_id: 1002929,
-    },
-  ]
-  const tt = flattenObjectToArray(dd)
-  console.log(tt, 'tttttttt')
-
   return {
     data: allItems,
   }
