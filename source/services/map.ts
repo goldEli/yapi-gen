81da2d7bb9d94493a8b3c2145b3a836d00829bd8
yangpeng <yangpeng@ifun.com>
2023-11-21 12:08:01 +0800

    feat: 添加数据

--- source/services/map.ts
+++ source/services/map.ts
@@ -1,13 +1,15 @@
 // 导图接口
 import { flattenObjectToArray } from '@/views/Encephalogram/until'
 import * as http from '../tools/http'
+import { addTaskForTable } from '@/views/Encephalogram/until/DbHelper'
 
 // 获取导图任务列表
 export const getMapList = async (params: any) => {
   const response = await http.get('getMapList', params)
   // 拆分树，存入indexDB
-  const arr = flattenObjectToArray(response.data)
-  console.log(arr, 'arrarrarrarrarrarrarr')
-
+  if (response.data) {
+    const arr = flattenObjectToArray(response.data)
+    addTaskForTable(params.project_id, arr)
+  }
   return response.data
 }
