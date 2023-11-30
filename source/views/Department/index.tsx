a0bf2bc53a564b04e43847436d3ee1b35f971fd4
zhaochunmei <zhaochunmei@ifun.com>
2023-11-30 16:23:07 +0800

    style: 成员职位和部门页面样式修改

--- source/views/Department/index.tsx
+++ source/views/Department/index.tsx
@@ -263,7 +263,7 @@ const Index = () => {
             </CommonButton>
           </OperateWrap>
         </HeaderWrap>
-        <TableWrap>
+        <TableWrap style={{height:'calc(100% - 90px)'}}>
           <ResizeTable
             isSpinning={isSpinning}
             dataWrapNormalHeight="calc(100% - 48px)"
@@ -277,7 +277,6 @@ const Index = () => {
             currentPage={page}
             pageSize={pageSize}
             onChange={(p, size) => {
-              console.log('----', p)
               setPage(p)
               setPageSize(size)
               _getList(p, size)
@@ -316,7 +315,7 @@ const Index = () => {
               })
             }
           >
-            <IconFont type="delete" />
+            删除
           </div>
         </Tooltip>
       </BatchAction>
--- source/views/Position/index.tsx
+++ source/views/Position/index.tsx
@@ -298,7 +298,7 @@ const Index = () => {
             </CommonButton>
           </OperateWrap>
         </HeaderWrap>
-        <TableWrap>
+        <TableWrap style={{height:'calc(100% - 90px)'}}>
           <ResizeTable
             isSpinning={isSpinning}
             dataWrapNormalHeight="calc(100% - 48px)"
@@ -312,7 +312,6 @@ const Index = () => {
             currentPage={page}
             pageSize={pageSize}
             onChange={(p, size) => {
-              console.log('----', p)
               setPage(p)
               setPageSize(size)
               _getList(p, size)
@@ -351,7 +350,7 @@ const Index = () => {
               })
             }
           >
-            <IconFont type="delete" />
+            删除
           </div>
         </Tooltip>
       </BatchAction>
