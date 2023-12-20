52276a43a62cf52bb5a3f0739269aa3239a6b39c
zhaochunmei <zhaochunmei@ifun.com>
2023-12-20 14:42:15 +0800

    fix: 拖

--- source/components/ProjectCardShow/style.tsx
+++ source/components/ProjectCardShow/style.tsx
@@ -37,13 +37,13 @@ export const CardRightFirst = styled.div`
 `
 
 export const CardRightSecond = styled.div`
-  height: 20px;
   font-size: 12px;
   font-family: PingFang SC-Regular, PingFang SC;
   font-weight: 400;
   color: var(--neutral-n3);
   line-height: 20px;
   margin-top:3px;
+  margin-bottom:8px;
 `
 
 export const TransformWrap = styled.div`
