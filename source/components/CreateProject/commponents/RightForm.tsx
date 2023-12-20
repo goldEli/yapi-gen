745989adccb170bf17f006d1f0e9aeaa1f284a60
zhaochunmei <zhaochunmei@ifun.com>
2023-12-20 12:30:26 +0800

    fix: 创建项目样式

--- source/components/CreateProject/commponents/RightForm.tsx
+++ source/components/CreateProject/commponents/RightForm.tsx
@@ -178,7 +178,7 @@ const RightForm = (props: {
                 <IconFont
                   style={{
                     position: 'absolute',
-                    left: '36px',
+                    left: localStorage.language === 'zh' ? '43px' :'72px',
                     top: '4px',
                     color: 'var(--neutral-n3)',
                   }}
@@ -249,6 +249,7 @@ const RightForm = (props: {
           name="info"
         >
           <Input.TextArea
+            style={{ marginBottom: 16 }}
             placeholder={t('please_enter_project_description')}
             autoSize={{ minRows: 3, maxRows: 5 }}
           />
@@ -264,7 +265,7 @@ const RightForm = (props: {
           </CommonButton>
         )}
         <CommonButton type="primary" onClick={onCreateProject}>
-          {props.isEditId ? '编辑':'创建'}
+          {props.isEditId ? '编辑' : '创建'}
         </CommonButton>
       </FooterBtn>
     </div>
