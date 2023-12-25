d396bbbc282b73a97211263ab39974985d5715b6
缪宇 <miaoyu2009@qq.com>
2023-12-25 09:55:40 +0800

    feat: iconpark

--- source/components/IconFont.tsx
+++ source/components/IconFont.tsx
@@ -4,7 +4,7 @@ import { createFromIconfontCN } from '@ant-design/icons'
 
 const IconFont = createFromIconfontCN({
   scriptUrl: [
-    'https://lf1-cdn-tos.bytegoofy.com/obj/iconpark/svg_24216_222.14eb4f2c270999df0a8ea0b18a620d7d.js',
+    'https://lf1-cdn-tos.bytegoofy.com/obj/iconpark/svg_24216_225.1065f5f2c5056aae69014d1fedb3856e.js',
   ],
 })
 
